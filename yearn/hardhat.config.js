ff742131280e050f0a0913d188277e8967e8710f
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2021-07-06 09:25:09 +0200

    Yearn submodule
    This change introduce a `yearn` submodule intended
    to be used as a bag for Yearn Finance integration
    components.

---
+++ yearn/hardhat.config.js
@@ -0,0 +1,12 @@
+require("@nomiclabs/hardhat-waffle")
+require("hardhat-gas-reporter")
+
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.6.12",
+      },
+    ],
+  },
+}
