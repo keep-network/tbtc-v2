9e3bd8c9fcb864e767195def01d3966c0fcc5f5a
Piotr Dyraga <piotr.dyraga@keep.network>
2021-07-09 11:12:10 +0200

    Renamed changeTimestamp to changeInitiatedTimestamp
    More specific name to clearly indicate what the parameter of
    GovernanceUtils functions stands for.

--- solidity/contracts/GovernanceUtils.sol
+++ solidity/contracts/GovernanceUtils.sol
@@ -6,34 +6,34 @@ library GovernanceUtils {
     /// @notice Reverts if the governance delay has not passed since
     ///         the change initiated time or if the change has not been
     ///         initiated.
-    /// @param changeTimestamp The timestamp at which the change has been
-    ///        initiated.
+    /// @param changeInitiatedTimestamp The timestamp at which the change has
+    ///        been initiated.
     /// @param delay Governance delay.
-    function onlyAfterGovernanceDelay(uint256 changeTimestamp, uint256 delay)
-        internal
-        view
-    {
-        require(changeTimestamp > 0, "Change not initiated");
+    function onlyAfterGovernanceDelay(
+        uint256 changeInitiatedTimestamp,
+        uint256 delay
+    ) internal view {
+        require(changeInitiatedTimestamp > 0, "Change not initiated");
         require(
             /* solhint-disable-next-line not-rely-on-time */
-            block.timestamp - changeTimestamp >= delay,
+            block.timestamp - changeInitiatedTimestamp >= delay,
             "Governance delay has not elapsed"
         );
     }
 
     /// @notice Gets the time remaining until the governable parameter update
     ///         can be committed.
-    /// @param changeTimestamp Timestamp indicating the beginning of the change
+    /// @param changeInitiatedTimestamp Timestamp indicating the beginning of
+    ///        the change
     /// @param delay Governance delay
     /// @return Remaining time in seconds
-    function getRemainingChangeTime(uint256 changeTimestamp, uint256 delay)
-        internal
-        view
-        returns (uint256)
-    {
-        require(changeTimestamp > 0, "Change not initiated");
+    function getRemainingChangeTime(
+        uint256 changeInitiatedTimestamp,
+        uint256 delay
+    ) internal view returns (uint256) {
+        require(changeInitiatedTimestamp > 0, "Change not initiated");
         /* solhint-disable-next-line not-rely-on-time */
-        uint256 elapsed = block.timestamp - changeTimestamp;
+        uint256 elapsed = block.timestamp - changeInitiatedTimestamp;
         if (elapsed >= delay) {
             return 0;
         } else {
