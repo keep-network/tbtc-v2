0488722a46b05b5ea806446aa93c16affc789cda
Piotr Dyraga <piotr.dyraga@keep.network>
2021-07-28 16:33:31 +0200

    Updated token symbol and name to tBTC/tBTC v2

--- solidity/contracts/token/TBTCToken.sol
+++ solidity/contracts/token/TBTCToken.sol
@@ -6,5 +6,5 @@ import "@thesis/solidity-contracts/contracts/token/ERC20WithPermit.sol";
 import "./MisfundRecovery.sol";
 
 contract TBTCToken is ERC20WithPermit, MisfundRecovery {
-    constructor() ERC20WithPermit("TBTC v2", "TBTC") {}
+    constructor() ERC20WithPermit("tBTC v2", "tBTC") {}
 }
