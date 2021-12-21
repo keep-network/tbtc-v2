6d55ae84ef6dd9e75ef53606568ea2aa48550cf3
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2021-12-21 12:19:04 +0100

    Deposit flow outline
    This changeset introduces an outline
    of the code responsible for constructing
    the BTC deposit transaction.

---
+++ tbtc-ts/src/bitcoin.ts
@@ -0,0 +1,97 @@
+/**
+ * Represents a raw transaction.
+ */
+export interface RawTransaction {
+  /**
+   * The full transaction payload as an un-prefixed hex string.
+   */
+  transactionHex: string
+}
+
+/**
+ * Data about a transaction.
+ */
+export interface Transaction {
+  /**
+   * The transaction hash (or transaction ID) as an un-prefixed hex string.
+   */
+  transactionHash: string
+
+  /**
+   * The vector of transaction inputs.
+   */
+  inputs: TransactionInput[]
+
+  /**
+   * The vector of transaction outputs.
+   */
+  outputs: TransactionOutput[]
+}
+
+/**
+ * Data about a transaction outpoint.
+ */
+export interface TransactionOutpoint {
+  /**
+   * The hash of the transaction the outpoint belongs to.
+   */
+  transactionHash: string
+
+  /**
+   * The zero-based index of the output from the specified transaction.
+   */
+  outputIndex: number
+}
+
+/**
+ * Data about a transaction input.
+ */
+export type TransactionInput = TransactionOutpoint & {
+  /**
+   * The scriptSig that unlocks the specified outpoint for spending.
+   */
+  scriptSig: any
+}
+
+/**
+ * Data about a transaction output.
+ */
+export interface TransactionOutput {
+  /**
+   * The 0-based index of the output.
+   */
+  outputIndex: number
+
+  /**
+   * The value of the output in satoshis.
+   */
+  value: number
+
+  /**
+   * The receiving scriptPubKey.
+   */
+  scriptPubKey: any
+}
+
+/**
+ * Data about an unspent transaction output.
+ */
+export type UnspentTransactionOutput = TransactionOutpoint & {
+  /**
+   * The unspent value in satoshis.
+   */
+  value: number
+}
+
+// TODO: Documentation.
+export interface Client {
+  findAllUnspentTransactionOutputs(
+    address: string
+  ): Promise<UnspentTransactionOutput[]>
+
+  getTransaction(transactionHash: string): Promise<Transaction>
+
+  getRawTransaction(transactionHash: string): Promise<RawTransaction>
+
+  broadcast(transaction: RawTransaction): Promise<void>
+}
--- tbtc-ts/src/deposit.ts
+++ tbtc-ts/src/deposit.ts
@@ -1,5 +1,99 @@
-export async function createDeposit(): Promise<void> {
-  // TODO: Implementation.
+import bcoin from "bcoin"
+import wif from "wif"
+import {
+  Client as BitcoinClient,
+  RawTransaction,
+  UnspentTransactionOutput,
+} from "./bitcoin"
+
+// TODO: Documentation
+export interface DepositData {
+  ethereumAddress: string
+  amount: number
+  refundPublicKey: string
+}
+
+// TODO: Documentation
+export async function createDeposit(
+  bitcoinClient: BitcoinClient,
+  depositorPrivateKey: string,
+  depositData: DepositData
+): Promise<void> {
+  const decodedDepositorPrivateKey = wif.decode(depositorPrivateKey)
+
+  const depositorKeyRing = new bcoin.KeyRing({
+    witness: true,
+    privateKey: decodedDepositorPrivateKey.privateKey,
+    compressed: decodedDepositorPrivateKey.compressed,
+  })
+
+  const depositorAddress = depositorKeyRing.getAddress("string")
+
+  const utxos = await bitcoinClient.findAllUnspentTransactionOutputs(
+    depositorAddress
+  )
+
+  const utxosWithRaw: (UnspentTransactionOutput & RawTransaction)[] = []
+  for (const utxo of utxos) {
+    const rawTransaction = await bitcoinClient.getRawTransaction(
+      utxo.transactionHash
+    )
+
+    utxosWithRaw.push({
+      ...utxo,
+      transactionHex: rawTransaction.transactionHex,
+    })
+  }
+
+  const rawUnsignedTransaction = await assembleDepositTransaction(
+    utxosWithRaw,
+    depositorAddress,
+    depositData
+  )
+
+  const unsignedTransaction = bcoin.MTX.fromRaw(
+    rawUnsignedTransaction.transactionHex,
+    "hex"
+  )
+  const signedTransaction = unsignedTransaction.sign(depositorKeyRing)
+
+  await bitcoinClient.broadcast({
+    transactionHex: signedTransaction.toRaw().toString("hex"),
+  })
+}
+
+// TODO: Documentation
+export async function assembleDepositTransaction(
+  utxos: (UnspentTransactionOutput & RawTransaction)[],
+  changeAddress: string,
+  depositData: DepositData
+): Promise<RawTransaction> {
+  const inputCoins = utxos.map((utxo) =>
+    bcoin.Coin.fromTX(
+      bcoin.MTX.fromRaw(utxo.transactionHex, "hex"),
+      utxo.outputIndex,
+      -1
+    )
+  )
+
+  // TODO: Fail fast if input coins sum is less than deposit amount.
+
+  const transaction = new bcoin.MTX()
+
+  transaction.addOutput({
+    script: {}, // TODO: Construct the script
+    value: depositData.amount,
+  })
+
+  await transaction.fund(inputCoins, {
+    rate: null, // set null explicitly to always use the default value
+    changeAddress: changeAddress,
+    subtractFee: false, // do not subtract the fee from outputs
+  })
+
+  return {
+    transactionHex: transaction.toRaw().toString("hex"),
+  }
 }
 
 export async function revealDeposit(): Promise<void> {
