5c9624eeb20ce583240e6282e755e171c20b162e
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2022-01-05 14:08:19 +0100

    Adjust bridge test data to LE locktime

--- solidity/test/bridge/Bridge.test.ts
+++ solidity/test/bridge/Bridge.test.ts
@@ -19,9 +19,9 @@ describe("Bridge", () => {
             "0x018348cdeb551134fe1f19d378a8adec9b146671cb67b945b71bf56b20d" +
             "c2b952f0100000000ffffffff",
           outputVector:
-            "0x02102700000000000017a914867120d5480a9cc0c11c1193fa59b3a92e852" +
-            "da7877ed73b00000000001600147ac2d9378a1c47e589dfb8095ca95ed2140d" +
-            "2726",
+            "0x02102700000000000017a9146ade1c799a3e5a59678e776f21be14d66dc" +
+            "15ed8877ed73b00000000001600147ac2d9378a1c47e589dfb8095ca95ed2" +
+            "140d2726",
           locktime: "0x00000000",
         },
         {
@@ -32,7 +32,7 @@ describe("Bridge", () => {
             "0x03989d253b17a6a0f41838b84ff0d20e8898f9d7b1a98f2564da4cc29dcf8581d9",
           refundPubKey:
             "0x0300d6f28a2f6bf9836f57fcda5d284c9a8f849316119779f0d6090830d97763a9",
-          refundLocktime: "0x61eabc60",
+          refundLocktime: "0x60bcea61",
         },
         "0x594cfd89700040163727828AE20B52099C58F02C"
       )
