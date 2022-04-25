2d20d7221d8ce6d4520a04fa90ff52432185b47f
Jakub Nowakowski <jakub.nowakowski@akena.co>
2022-04-25 15:13:49 +0200

    Implement deployment scripts for Bank and Bridge
    Here we implement hardhat deployment script for Bank and Bridge
    contracts.
    
    Bridge contract requires a reference to WalletRegistry, so we added an
    external deploy scripts definition in hardhat config.

---
+++ solidity/deploy/05_deploy_bridge.ts
@@ -0,0 +1,60 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types"
+import { DeployFunction } from "hardhat-deploy/types"
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployments, getNamedAccounts } = hre
+  const { deploy } = deployments
+  const { deployer, treasury } = await getNamedAccounts()
+
+  const Bank = await deployments.get("Bank")
+  const Relay = await deployments.get("Relay")
+
+  // TODO: Test for mainnet deployment that when `WalletRegistry` is provided
+  // in `external/mainnet/` directory it gets resolved correctly, and the deployment
+  // script from `@keep-network/ecdsa` is not invoked once again.
+  const WalletRegistry = await deployments.get("WalletRegistry")
+
+  const txProofDifficultyFactor = 0 // TODO: Set initial value
+
+  const Deposit = await deploy("Deposit", { from: deployer, log: true })
+  const Sweep = await deploy("Sweep", { from: deployer, log: true })
+  const Redemption = await deploy("Redemption", { from: deployer, log: true })
+  const Wallets = await deploy("Wallets", { from: deployer, log: true })
+  const Fraud = await deploy("Fraud", { from: deployer, log: true })
+  const MovingFunds = await deploy("MovingFunds", {
+    from: deployer,
+    log: true,
+  })
+
+  const Bridge = await deploy("Bridge", {
+    from: deployer,
+    args: [
+      Bank.address,
+      Relay.address,
+      treasury,
+      WalletRegistry.address,
+      txProofDifficultyFactor,
+    ],
+    libraries: {
+      Deposit: Deposit.address,
+      Sweep: Sweep.address,
+      Redemption: Redemption.address,
+      Wallets: Wallets.address,
+      Fraud: Fraud.address,
+      MovingFunds: MovingFunds.address,
+    },
+    log: true,
+  })
+
+  if (hre.network.tags.tenderly) {
+    await hre.tenderly.verify({
+      name: "Bridge",
+      address: Bridge.address,
+    })
+  }
+}
+
+export default func
+
+func.tags = ["Bridge"]
+func.dependencies = ["Bank", "Relay", "Treasury", "WalletRegistry"]
