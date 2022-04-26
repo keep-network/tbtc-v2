7ccaf3d27bdc9b4397d5f8b1e49d28a18bba1832
Tomasz Slabon <tomasz.slabon@keep.network>
2022-04-26 18:16:49 +0200

    Implemented updating redemption related parameters

--- solidity/test/fixtures/index.ts
+++ solidity/test/fixtures/index.ts
@@ -17,6 +17,10 @@ export const constants = {
   walletMaxBtcBalance: to1ePrecision(10, 8), // 10 BTC
   walletMaxAge: 8 * 604800, // 8 weeks,
   walletMaxBtcTransfer: to1ePrecision(10, 8), // 10 BTC
+  redemptionDustThreshold: 1000000, // 1000000 satoshi = 0.01 BTC
+  redemptionTreasuryFeeDivisor: 2000, // 1/2000 == 5bps == 0.05% == 0.0005
+  redemptionTxMaxFee: 10000, // 10000 satoshi
+  redemptionTimeout: 172800, // 48 hours
 }
 
 export const walletState = {
