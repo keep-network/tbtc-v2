164f5c2440eef48629429e189cfc9cbfe14d51b3
Dmitry <dymitr.paremski@keep.network>
2022-05-06 13:50:06 +0200

    Running deployment of Reimbursement Pool only for hardhat testing purposes

--- solidity/deploy/07_deploy_reimbursement_pool.ts
+++ solidity/deploy/07_deploy_reimbursement_pool.ts
@@ -9,28 +9,27 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const staticGas = 40800 // gas amount consumed by the refund() + tx cost
   const maxGasPrice = ethers.utils.parseUnits("500", "gwei")
 
-  const ReimbursementPool = await deploy("ReimbursementPool", {
-    contract:
-      deployments.getNetworkName() === "hardhat"
-        ? "ReimbursementPool"
-        : undefined,
-    from: deployer,
-    args: [staticGas, maxGasPrice],
-    log: true,
-  })
-
-  const deployerSigner = await ethers.getSigner(deployer)
-
-  await deployerSigner.sendTransaction({
-    to: ReimbursementPool.address,
-    value: ethers.utils.parseEther("100.0"), // Send 100.0 ETH
-  })
-
-  if (hre.network.tags.tenderly) {
-    await hre.tenderly.verify({
-      name: "ReimbursementPool",
-      address: ReimbursementPool.address,
+  if (deployments.getNetworkName() === "hardhat") {
+    const ReimbursementPool = await deploy("ReimbursementPool", {
+      contract: "ReimbursementPool",
+      from: deployer,
+      args: [staticGas, maxGasPrice],
+      log: true,
     })
+  
+    const deployerSigner = await ethers.getSigner(deployer)
+  
+    await deployerSigner.sendTransaction({
+      to: ReimbursementPool.address,
+      value: ethers.utils.parseEther("100.0"), // Send 100.0 ETH
+    })
+  
+    if (hre.network.tags.tenderly) {
+      await hre.tenderly.verify({
+        name: "ReimbursementPool",
+        address: ReimbursementPool.address,
+      })
+    }
   }
 }
 
