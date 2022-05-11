375ae3344f8077b31e82be2ac300378715ca7645
Dmitry <dymitr.paremski@keep.network>
2022-05-11 13:03:16 +0200

    Rearranging the functions to align with the Bridge order

--- solidity/contracts/maintainer/MaintainerProxy.sol
+++ solidity/contracts/maintainer/MaintainerProxy.sol
@@ -102,24 +102,6 @@ contract MaintainerProxy is Ownable, Reimbursable {
         _submitMovingFundsCommitmentGasOffset = 8000;
     }
 
-    /// @notice Wraps request new wallet call and reimburses a caller's
-    ///         transaction cost.
-    /// @param activeWalletMainUtxo Data of the active wallet's main UTXO, as
-    ///        currently known on the Ethereum chain.
-    function requestNewWallet(BitcoinTx.UTXO calldata activeWalletMainUtxo)
-        external
-        onlyMaintainer
-    {
-        uint256 gasStart = gasleft();
-
-        bridge.requestNewWallet(activeWalletMainUtxo);
-
-        reimbursementPool.refund(
-            (gasStart - gasleft()) + _requestNewWalletGasOffset,
-            msg.sender
-        );
-    }
-
     /// @notice Wraps submit sweep proof call and reimburses a caller's
     ///         transaction cost.
     /// @param sweepTx Bitcoin sweep transaction data
@@ -172,57 +154,38 @@ contract MaintainerProxy is Ownable, Reimbursable {
         );
     }
 
-    /// @notice Notifies that the wallet is either old enough or has too few
-    ///         satoshis left and qualifies to be closed.
-    /// @param walletPubKeyHash 20-byte public key hash of the wallet
-    /// @param walletMainUtxo Data of the wallet's main UTXO, as currently
-    ///        known on the Ethereum chain.
-    function notifyCloseableWallet(
+    /// @notice Submits the moving funds target wallets commitment.
+    ///         Once all requirements are met, that function registers the
+    ///         target wallets commitment and opens the way for moving funds
+    ///         proof submission.
+    /// @param walletPubKeyHash 20-byte public key hash of the source wallet
+    /// @param walletMainUtxo Data of the source wallet's main UTXO, as
+    ///        currently known on the Ethereum chain
+    /// @param walletMembersIDs Identifiers of the source wallet signing group
+    ///        members
+    /// @param walletMemberIndex Position of the caller in the source wallet
+    ///        signing group members list
+    /// @param targetWallets List of 20-byte public key hashes of the target
+    ///        wallets that the source wallet commits to move the funds to
+    function submitMovingFundsCommitment(
         bytes20 walletPubKeyHash,
-        BitcoinTx.UTXO calldata walletMainUtxo
+        BitcoinTx.UTXO calldata walletMainUtxo,
+        uint32[] calldata walletMembersIDs,
+        uint256 walletMemberIndex,
+        bytes20[] calldata targetWallets
     ) external onlyMaintainer {
         uint256 gasStart = gasleft();
 
-        bridge.notifyCloseableWallet(walletPubKeyHash, walletMainUtxo);
-
-        reimbursementPool.refund(
-            (gasStart - gasleft()) + _notifyCloseableWalletGasOffset,
-            msg.sender
+        bridge.submitMovingFundsCommitment(
+            walletPubKeyHash,
+            walletMainUtxo,
+            walletMembersIDs,
+            walletMemberIndex,
+            targetWallets
         );
-    }
-
-    /// @notice Allows to defeat a pending fraud challenge against a wallet if
-    ///         the transaction that spends the UTXO follows the protocol rules.
-    ///         In order to defeat the challenge the same `walletPublicKey` and
-    ///         signature (represented by `r`, `s` and `v`) must be provided as
-    ///         were used to calculate the sighash during input signing.
-    ///         The fraud challenge defeat attempt will only succeed if the
-    ///         inputs in the preimage are considered honestly spent by the
-    ///         wallet. Therefore the transaction spending the UTXO must be
-    ///         proven in the Bridge before a challenge defeat is called.
-    ///         If successfully defeated, the fraud challenge is marked as
-    ///         resolved and the amount of ether deposited by the challenger is
-    ///         sent to the treasury.
-    /// @param walletPublicKey The public key of the wallet in the uncompressed
-    ///        and unprefixed format (64 bytes)
-    /// @param preimage The preimage which produces sighash used to generate the
-    ///        ECDSA signature that is the subject of the fraud claim. It is a
-    ///        serialized subset of the transaction. The exact subset used as
-    ///        the preimage depends on the transaction input the signature is
-    ///        produced for. See BIP-143 for reference
-    /// @param witness Flag indicating whether the preimage was produced for a
-    ///        witness input. True for witness, false for non-witness input
-    function defeatFraudChallenge(
-        bytes calldata walletPublicKey,
-        bytes calldata preimage,
-        bool witness
-    ) external onlyMaintainer {
-        uint256 gasStart = gasleft();
-
-        bridge.defeatFraudChallenge(walletPublicKey, preimage, witness);
 
         reimbursementPool.refund(
-            (gasStart - gasleft()) + _defeatFraudChallengeGasOffset,
+            (gasStart - gasleft()) + _submitMovingFundsCommitmentGasOffset,
             msg.sender
         );
     }
@@ -267,41 +230,86 @@ contract MaintainerProxy is Ownable, Reimbursable {
         );
     }
 
-    /// @notice Submits the moving funds target wallets commitment.
-    ///         Once all requirements are met, that function registers the
-    ///         target wallets commitment and opens the way for moving funds
-    ///         proof submission.
-    /// @param walletPubKeyHash 20-byte public key hash of the source wallet
-    /// @param walletMainUtxo Data of the source wallet's main UTXO, as
-    ///        currently known on the Ethereum chain
-    /// @param walletMembersIDs Identifiers of the source wallet signing group
-    ///        members
-    /// @param walletMemberIndex Position of the caller in the source wallet
-    ///        signing group members list
-    /// @param targetWallets List of 20-byte public key hashes of the target
-    ///        wallets that the source wallet commits to move the funds to
-    function submitMovingFundsCommitment(
-        bytes20 walletPubKeyHash,
-        BitcoinTx.UTXO calldata walletMainUtxo,
-        uint32[] calldata walletMembersIDs,
-        uint256 walletMemberIndex,
-        bytes20[] calldata targetWallets
-    ) external onlyMaintainer {
+    // TODO: add notifyMovingFundsBelowDust
+
+    // TODO: add submitMovedFundsSweepProof
+
+    /// @notice Wraps request new wallet call and reimburses a caller's
+    ///         transaction cost.
+    /// @param activeWalletMainUtxo Data of the active wallet's main UTXO, as
+    ///        currently known on the Ethereum chain.
+    function requestNewWallet(BitcoinTx.UTXO calldata activeWalletMainUtxo)
+        external
+        onlyMaintainer
+    {
         uint256 gasStart = gasleft();
 
-        bridge.submitMovingFundsCommitment(
-            walletPubKeyHash,
-            walletMainUtxo,
-            walletMembersIDs,
-            walletMemberIndex,
-            targetWallets
-        );
+        bridge.requestNewWallet(activeWalletMainUtxo);
 
         reimbursementPool.refund(
-            (gasStart - gasleft()) + _submitMovingFundsCommitmentGasOffset,
+            (gasStart - gasleft()) + _requestNewWalletGasOffset,
             msg.sender
         );
     }
+
+    /// @notice Notifies that the wallet is either old enough or has too few
+    ///         satoshis left and qualifies to be closed.
+    /// @param walletPubKeyHash 20-byte public key hash of the wallet
+    /// @param walletMainUtxo Data of the wallet's main UTXO, as currently
+    ///        known on the Ethereum chain.
+    function notifyCloseableWallet(
+        bytes20 walletPubKeyHash,
+        BitcoinTx.UTXO calldata walletMainUtxo
+    ) external onlyMaintainer {
+        uint256 gasStart = gasleft();
+
+        bridge.notifyCloseableWallet(walletPubKeyHash, walletMainUtxo);
+
+        reimbursementPool.refund(
+            (gasStart - gasleft()) + _notifyCloseableWalletGasOffset,
+            msg.sender
+        );
+    }
+
+    // TODO: add notifyWalletClosingPeriodElapsed
+
+    /// @notice Allows to defeat a pending fraud challenge against a wallet if
+    ///         the transaction that spends the UTXO follows the protocol rules.
+    ///         In order to defeat the challenge the same `walletPublicKey` and
+    ///         signature (represented by `r`, `s` and `v`) must be provided as
+    ///         were used to calculate the sighash during input signing.
+    ///         The fraud challenge defeat attempt will only succeed if the
+    ///         inputs in the preimage are considered honestly spent by the
+    ///         wallet. Therefore the transaction spending the UTXO must be
+    ///         proven in the Bridge before a challenge defeat is called.
+    ///         If successfully defeated, the fraud challenge is marked as
+    ///         resolved and the amount of ether deposited by the challenger is
+    ///         sent to the treasury.
+    /// @param walletPublicKey The public key of the wallet in the uncompressed
+    ///        and unprefixed format (64 bytes)
+    /// @param preimage The preimage which produces sighash used to generate the
+    ///        ECDSA signature that is the subject of the fraud claim. It is a
+    ///        serialized subset of the transaction. The exact subset used as
+    ///        the preimage depends on the transaction input the signature is
+    ///        produced for. See BIP-143 for reference
+    /// @param witness Flag indicating whether the preimage was produced for a
+    ///        witness input. True for witness, false for non-witness input
+    function defeatFraudChallenge(
+        bytes calldata walletPublicKey,
+        bytes calldata preimage,
+        bool witness
+    ) external onlyMaintainer {
+        uint256 gasStart = gasleft();
+
+        bridge.defeatFraudChallenge(walletPublicKey, preimage, witness);
+
+        reimbursementPool.refund(
+            (gasStart - gasleft()) + _defeatFraudChallengeGasOffset,
+            msg.sender
+        );
+    }
+
+    // TODO: add defeatFraudChallengeWithHeartbeat
 
     /// @notice Authorize a maintainer that can interact with this reimbursment pool.
     ///         Can be authorized by the owner only.
