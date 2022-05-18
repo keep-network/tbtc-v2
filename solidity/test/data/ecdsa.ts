82260bde013a053df22a01ec9b485247cabcf500
Jakub Nowakowski <jakub.nowakowski@akena.co>
2022-05-18 13:18:25 +0200

    Add concatenated public key to ecdsa data

--- solidity/test/data/ecdsa.ts
+++ solidity/test/data/ecdsa.ts
@@ -15,6 +15,11 @@ export const ecdsaWalletTestData = {
     "0x2cd470243453a299fa9e77237716103abc11a1df38855ed6f2ee187e9c582ba6"
   ),
 
+  // public key (X and Y coordinates concatenation)
+  publicKey: <BytesLike>(
+    "0x50863ad64a87ae8a2fe83c1af1a8403cb53f53e486d8511dad8a04887e5b23522cd470243453a299fa9e77237716103abc11a1df38855ed6f2ee187e9c582ba6"
+  ),
+
   // compressed public key
   compressedPublicKey: <BytesLike>(
     "0x0250863ad64a87ae8a2fe83c1af1a8403cb53f53e486d8511dad8a04887e5b2352"
