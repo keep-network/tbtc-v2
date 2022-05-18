34dbb1e579c00d3c416db7b148bc880b2ec0ff09
Jakub Nowakowski <jakub.nowakowski@akena.co>
2022-05-18 15:07:01 +0200

    Add integration test for wallet creation
    The test integrates bridge with wallet registry and invokes new wallet
    creation.

---
+++ solidity/test/integration/integration.test.ts
@@ -0,0 +1,149 @@
+/* eslint-disable no-await-in-loop */
+import { deployments, ethers, helpers } from "hardhat"
+import { expect } from "chai"
+
+import type { FakeContract } from "@defi-wonderland/smock"
+import type { BigNumberish, Contract } from "ethers"
+import type { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers"
+import type { Bridge, IRandomBeacon, WalletRegistry } from "../../typechain"
+
+import {
+  registerOperator,
+  produceEcdsaDkgResult,
+} from "./utils/ecdsa-wallet-registry"
+import { ecdsaWalletTestData } from "../data/ecdsa"
+import { fakeRandomBeacon, produceRelayEntry } from "./utils/random-beacon"
+import { authorizeApplication, stake } from "./utils/staking"
+
+const { to1e18 } = helpers.number
+
+const NO_MAIN_UTXO = {
+  txHash: "0x0000000000000000000000000000000000000000000000000000000000000000",
+  txOutputIndex: 0,
+  txOutputValue: 0,
+}
+
+const describeFn =
+  process.env.NODE_ENV === "integration-test" ? describe : describe.skip
+
+describeFn("Integration Test", async () => {
+  let bridge: Bridge
+  let walletRegistry: WalletRegistry
+  let t: Contract
+  let staking: Contract
+  let randomBeacon: FakeContract<IRandomBeacon>
+  let governance: SignerWithAddress
+
+  // Number of operators to register in the sortition pool
+  const numberOfOperators = 110
+  // Number of ECDSA wallet members to select
+  const groupSize = 100
+
+  const unnamedSignersOffset = 0
+  const stakeAmount = to1e18(40_000)
+  const dkgResultChallengePeriodLength = 10
+
+  // TODO: Generate a random public key
+  const ecdsaGroupPublicKey = ecdsaWalletTestData.publicKey
+  const walletPubKeyHash = ecdsaWalletTestData.pubKeyHash160
+
+  before(async () => {
+    await deployments.fixture()
+    ;({ governance } = await helpers.signers.getNamedSigners())
+
+    bridge = await helpers.contracts.getContract("Bridge")
+    walletRegistry = await helpers.contracts.getContract("WalletRegistry")
+    t = await helpers.contracts.getContract("T")
+    staking = await helpers.contracts.getContract("TokenStaking")
+
+    // TODO: INTEGRATE WITH THE REAL BEACON
+    randomBeacon = await fakeRandomBeacon(walletRegistry)
+
+    // Update only the parameters that are crucial for this test.
+    await updateWalletRegistryParams()
+
+    const signers = (await helpers.signers.getUnnamedSigners()).slice(
+      unnamedSignersOffset
+    )
+
+    // We use unique accounts for each staking role for each operator.
+    if (signers.length < numberOfOperators * 5) {
+      throw new Error(
+        "not enough unnamed signers; update hardhat network's configuration account count"
+      )
+    }
+
+    for (let i = 0; i < numberOfOperators; i++) {
+      const owner: SignerWithAddress = signers[i]
+      const stakingProvider: SignerWithAddress =
+        signers[1 * numberOfOperators + i]
+      const operator: SignerWithAddress = signers[2 * numberOfOperators + i]
+      const beneficiary: SignerWithAddress = signers[3 * numberOfOperators + i]
+      const authorizer: SignerWithAddress = signers[4 * numberOfOperators + i]
+
+      await stake(
+        t,
+        staking,
+        stakeAmount,
+        owner,
+        stakingProvider.address,
+        beneficiary.address,
+        authorizer.address
+      )
+      await authorizeApplication(
+        staking,
+        walletRegistry.address,
+        authorizer,
+        stakingProvider.address,
+        stakeAmount
+      )
+      await registerOperator(walletRegistry, stakingProvider, operator)
+    }
+  })
+
+  it("new wallet creation (happy path)", async () => {
+    expect(await bridge.activeWalletPubKeyHash()).to.be.equal(
+      ethers.constants.AddressZero
+    )
+
+    const { blockNumber: startBlock } = await bridge.requestNewWallet(
+      NO_MAIN_UTXO
+    )
+
+    const relayEntry: BigNumberish = await produceRelayEntry(
+      walletRegistry,
+      randomBeacon
+    )
+
+    await produceEcdsaDkgResult(
+      walletRegistry,
+      ecdsaGroupPublicKey,
+      relayEntry,
+      startBlock,
+      groupSize
+    )
+
+    expect(await bridge.activeWalletPubKeyHash()).to.be.equal(walletPubKeyHash)
+  })
+
+  async function updateWalletRegistryParams() {
+    const walletRegistryGovernance = await ethers.getContractAt(
+      (
+        await deployments.getArtifact("WalletRegistryGovernance")
+      ).abi,
+      await walletRegistry.governance()
+    )
+
+    await walletRegistryGovernance
+      .connect(governance)
+      .beginDkgResultChallengePeriodLengthUpdate(dkgResultChallengePeriodLength)
+
+    await helpers.time.increaseTime(
+      await walletRegistryGovernance.governanceDelay()
+    )
+
+    await walletRegistryGovernance
+      .connect(governance)
+      .finalizeDkgResultChallengePeriodLengthUpdate()
+  }
+})
