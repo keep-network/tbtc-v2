ac4f4b51887c3d235dd14141c79a0d6fad365c61
Piotr Dyraga <piotr.dyraga@keep.network>
2022-05-26 15:35:10 +0200

    Adjusted redemptionTimeout type in Redemption.test.ts
    After changing RedemptionTimeout to be `uint32` in contracts, we can use
    just `number` type to represent it in unit tests.

--- solidity/test/bridge/Bridge.Redemption.test.ts
+++ solidity/test/bridge/Bridge.Redemption.test.ts
@@ -57,7 +57,7 @@ describe("Bridge - Redemption", () => {
   let bridge: Bridge & BridgeStub
   let walletRegistry: FakeContract<IWalletRegistry>
 
-  let redemptionTimeout: BigNumber
+  let redemptionTimeout: number
   let redemptionTimeoutSlashingAmount: BigNumber
   let redemptionTimeoutNotifierRewardMultiplier: number
 
@@ -4198,7 +4198,7 @@ describe("Bridge - Redemption", () => {
               data.redemptionRequests[0].amount
             )
 
-          await increaseTime(redemptionTimeout.sub(1))
+          await increaseTime(redemptionTimeout - 1)
         })
 
         after(async () => {
