2a681810ea7eb4784a73d3ae2ebb4bcdbe7aef09
Dmitry <dymitr.paremski@keep.network>
2022-05-27 11:27:35 +0200

    Improving contract docs
    - Clarified description for isAuthorized mapping
    - Referencing Bridge functions description from wrapper functions

--- solidity/contracts/maintainer/MaintainerProxy.sol
+++ solidity/contracts/maintainer/MaintainerProxy.sol
@@ -30,8 +30,9 @@ import "../bridge/Bridge.sol";
 contract MaintainerProxy is Ownable, Reimbursable {
     Bridge public bridge;
 
-    /// @notice Authorized maintainer that can interact with the maintainer proxy
-    ///         contract. Authorization can be granted and removed by the governance.
+    /// @notice Authorized maintainers that can interact with the set of functions
+    ///         for maintainers only. Authorization can be granted and removed by
+    ///         the governance.
     /// @dev    'Key' is the address of the maintainer. 'Value' represents an index+1
     ///         in the 'maintainers' array. 1 was added so the maintainer index can
     ///         never be 0 which is a reserved index for a non-existent maintainer
@@ -151,13 +152,9 @@ contract MaintainerProxy is Ownable, Reimbursable {
         _defeatFraudChallengeWithHeartbeatGasOffset = 5000;
     }
 
-    /// @notice Wraps submit deposit sweep proof call and reimburses a caller's
-    ///         transaction cost.
-    /// @param sweepTx Bitcoin sweep transaction data
-    /// @param sweepProof Bitcoin sweep proof data
-    /// @param mainUtxo Data of the wallet's main UTXO, as currently known on
-    ///        the Ethereum chain. If no main UTXO exists for the given wallet,
-    ///        this parameter is ignored
+    /// @notice Wraps `Bridge.submitDepositSweepProof` call and reimburses the
+    ///         caller's transaction cost.
+    /// @dev See `Bridge.submitDepositSweepProof` function documentation.
     function submitDepositSweepProof(
         BitcoinTx.Info calldata sweepTx,
         BitcoinTx.Proof calldata sweepProof,
@@ -174,15 +171,9 @@ contract MaintainerProxy is Ownable, Reimbursable {
         );
     }
 
-    /// @notice Wraps submit redemption proof call and reimburses a caller's
-    ///         transaction cost.
-    /// @param redemptionTx Bitcoin redemption transaction data
-    /// @param redemptionProof Bitcoin redemption proof data
-    /// @param mainUtxo Data of the wallet's main UTXO, as currently known on
-    ///        the Ethereum chain
-    /// @param walletPubKeyHash 20-byte public key hash (computed using Bitcoin
-    ///        HASH160 over the compressed ECDSA public key) of the wallet which
-    ///        performed the redemption transaction
+    /// @notice Wraps `Bridge.submitRedemptionProof` call and reimburses the
+    ///         caller's transaction cost.
+    /// @dev See `Bridge.submitRedemptionProof` function documentation.
     function submitRedemptionProof(
         BitcoinTx.Info calldata redemptionTx,
         BitcoinTx.Proof calldata redemptionProof,
@@ -204,19 +195,9 @@ contract MaintainerProxy is Ownable, Reimbursable {
         );
     }
 
-    /// @notice Wraps submit the moving funds target wallets commitment.
-    ///         Once all requirements are met, that function registers the
-    ///         target wallets commitment and opens the way for moving funds
-    ///         proof submission.
-    /// @param walletPubKeyHash 20-byte public key hash of the source wallet
-    /// @param walletMainUtxo Data of the source wallet's main UTXO, as
-    ///        currently known on the Ethereum chain
-    /// @param walletMembersIDs Identifiers of the source wallet signing group
-    ///        members
-    /// @param walletMemberIndex Position of the caller in the source wallet
-    ///        signing group members list
-    /// @param targetWallets List of 20-byte public key hashes of the target
-    ///        wallets that the source wallet commits to move the funds to
+    /// @notice Wraps `Bridge.submitMovingFundsCommitment` call and reimburses the
+    ///         caller's transaction cost.
+    /// @dev See `Bridge.submitMovingFundsCommitment` function documentation.
     function submitMovingFundsCommitment(
         bytes20 walletPubKeyHash,
         BitcoinTx.UTXO calldata walletMainUtxo,
@@ -240,10 +221,9 @@ contract MaintainerProxy is Ownable, Reimbursable {
         );
     }
 
-    /// @notice Wraps reset the moving funds timeout for the given wallet if the
-    ///         target wallet commitment cannot be submitted due to a lack
-    ///         of live wallets in the system.
-    /// @param walletPubKeyHash 20-byte public key hash of the moving funds wallet.
+    /// @notice Wraps `Bridge.resetMovingFundsTimeout` call and reimburses the
+    ///         caller's transaction cost.
+    /// @dev See `Bridge.resetMovingFundsTimeout` function documentation.
     function resetMovingFundsTimeout(bytes20 walletPubKeyHash) external {
         uint256 gasStart = gasleft();
 
@@ -255,25 +235,9 @@ contract MaintainerProxy is Ownable, Reimbursable {
         );
     }
 
-    /// @notice Wraps submit moving funds which is used by the wallet to prove
-    ///         the BTC moving funds transaction and to make the necessary state
-    ///         changes. Moving funds is only accepted if it satisfies SPV proof.
-    ///
-    ///         The function validates the moving funds transaction structure
-    ///         by checking if it actually spends the main UTXO of the declared
-    ///         wallet and locks the value on the pre-committed target wallets
-    ///         using a reasonable transaction fee. If all preconditions are
-    ///         met, this functions closes the source wallet.
-    ///
-    ///         It is possible to prove the given moving funds transaction only
-    ///         one time.
-    /// @param movingFundsTx Bitcoin moving funds transaction data
-    /// @param movingFundsProof Bitcoin moving funds proof data
-    /// @param mainUtxo Data of the wallet's main UTXO, as currently known on
-    ///        the Ethereum chain
-    /// @param walletPubKeyHash 20-byte public key hash (computed using Bitcoin
-    ///        HASH160 over the compressed ECDSA public key) of the wallet
-    ///        which performed the moving funds transaction
+    /// @notice Wraps `Bridge.submitMovingFundsProof` call and reimburses the
+    ///         caller's transaction cost.
+    /// @dev See `Bridge.submitMovingFundsProof` function documentation.
     function submitMovingFundsProof(
         BitcoinTx.Info calldata movingFundsTx,
         BitcoinTx.Proof calldata movingFundsProof,
@@ -295,12 +259,9 @@ contract MaintainerProxy is Ownable, Reimbursable {
         );
     }
 
-    /// @notice Wraps notify about a moving funds wallet whose BTC balance is
-    ///         below the moving funds dust threshold. Ends the moving funds
-    ///         process and begins wallet closing immediately.
-    /// @param walletPubKeyHash 20-byte public key hash of the wallet
-    /// @param mainUtxo Data of the wallet's main UTXO, as currently known
-    ///        on the Ethereum chain.
+    /// @notice Wraps `Bridge.notifyMovingFundsBelowDust` call and reimburses the
+    ///         caller's transaction cost.
+    /// @dev See `Bridge.notifyMovingFundsBelowDust` function documentation.
     function notifyMovingFundsBelowDust(
         bytes20 walletPubKeyHash,
         BitcoinTx.UTXO calldata mainUtxo
@@ -315,25 +276,9 @@ contract MaintainerProxy is Ownable, Reimbursable {
         );
     }
 
-    /// @notice Wraps submit moved funds sweep proof which is used by the wallet
-    ///         to prove the BTC moved funds sweep transaction and to make the
-    ///         necessary state changes. Moved funds sweep is only accepted if
-    ///         it satisfies SPV proof.
-    ///
-    ///         The function validates the sweep transaction structure by
-    ///         checking if it actually spends the moved funds UTXO and the
-    ///         sweeping wallet's main UTXO (optionally), and if it locks the
-    ///         value on the sweeping wallet's 20-byte public key hash using a
-    ///         reasonable transaction fee. If all preconditions are
-    ///         met, this function updates the sweeping wallet main UTXO, thus
-    ///         their BTC balance.
-    ///
-    ///         It is possible to prove the given sweep transaction only
-    ///         one time.
-    /// @param sweepTx Bitcoin sweep funds transaction data
-    /// @param sweepProof Bitcoin sweep funds proof data
-    /// @param mainUtxo Data of the sweeping wallet's main UTXO, as currently
-    ///        known on the Ethereum chain
+    /// @notice Wraps `Bridge.submitMovedFundsSweepProof` call and reimburses the
+    ///         caller's transaction cost.
+    /// @dev See `Bridge.submitMovedFundsSweepProof` function documentation.
     function submitMovedFundsSweepProof(
         BitcoinTx.Info calldata sweepTx,
         BitcoinTx.Proof calldata sweepProof,
@@ -349,10 +294,9 @@ contract MaintainerProxy is Ownable, Reimbursable {
         );
     }
 
-    /// @notice Wraps request new wallet call and reimburses a caller's
-    ///         transaction cost.
-    /// @param activeWalletMainUtxo Data of the active wallet's main UTXO, as
-    ///        currently known on the Ethereum chain.
+    /// @notice Wraps `Bridge.requestNewWallet` call and reimburses the
+    ///         caller's transaction cost.
+    /// @dev See `Bridge.requestNewWallet` function documentation.
     function requestNewWallet(BitcoinTx.UTXO calldata activeWalletMainUtxo)
         external
         onlyMaintainer
@@ -367,11 +311,9 @@ contract MaintainerProxy is Ownable, Reimbursable {
         );
     }
 
-    /// @notice Wraps notify that the wallet is either old enough or has too few
-    ///         satoshis left and qualifies to be closed.
-    /// @param walletPubKeyHash 20-byte public key hash of the wallet
-    /// @param walletMainUtxo Data of the wallet's main UTXO, as currently
-    ///        known on the Ethereum chain.
+    /// @notice Wraps `Bridge.notifyWalletCloseable` call and reimburses the
+    ///         caller's transaction cost.
+    /// @dev See `Bridge.notifyWalletCloseable` function documentation.
     function notifyWalletCloseable(
         bytes20 walletPubKeyHash,
         BitcoinTx.UTXO calldata walletMainUtxo
@@ -386,10 +328,9 @@ contract MaintainerProxy is Ownable, Reimbursable {
         );
     }
 
-    /// @notice Wraps notify about the end of the closing period for the given wallet.
-    ///         Closes the wallet ultimately and notifies the ECDSA registry
-    ///         about this fact.
-    /// @param walletPubKeyHash 20-byte public key hash of the wallet
+    /// @notice Wraps `Bridge.notifyWalletClosingPeriodElapsed` call and reimburses
+    ///         the caller's transaction cost.
+    /// @dev See `Bridge.notifyWalletClosingPeriodElapsed` function documentation.
     function notifyWalletClosingPeriodElapsed(bytes20 walletPubKeyHash)
         external
         onlyMaintainer
@@ -404,28 +345,9 @@ contract MaintainerProxy is Ownable, Reimbursable {
         );
     }
 
-    /// @notice Wraps defeat fraud challenge that allows to defeat a pending
-    ///         fraud challenge against a wallet if the transaction that spends
-    ///         the UTXO follows the protocol rules.
-    ///         In order to defeat the challenge the same `walletPublicKey` and
-    ///         signature (represented by `r`, `s` and `v`) must be provided as
-    ///         were used to calculate the sighash during input signing.
-    ///         The fraud challenge defeat attempt will only succeed if the
-    ///         inputs in the preimage are considered honestly spent by the
-    ///         wallet. Therefore the transaction spending the UTXO must be
-    ///         proven in the Bridge before a challenge defeat is called.
-    ///         If successfully defeated, the fraud challenge is marked as
-    ///         resolved and the amount of ether deposited by the challenger is
-    ///         sent to the treasury.
-    /// @param walletPublicKey The public key of the wallet in the uncompressed
-    ///        and unprefixed format (64 bytes)
-    /// @param preimage The preimage which produces sighash used to generate the
-    ///        ECDSA signature that is the subject of the fraud claim. It is a
-    ///        serialized subset of the transaction. The exact subset used as
-    ///        the preimage depends on the transaction input the signature is
-    ///        produced for. See BIP-143 for reference
-    /// @param witness Flag indicating whether the preimage was produced for a
-    ///        witness input. True for witness, false for non-witness input
+    /// @notice Wraps `Bridge.defeatFraudChallenge` call and reimburses the
+    ///         caller's transaction cost.
+    /// @dev See `Bridge.defeatFraudChallenge` function documentation.
     function defeatFraudChallenge(
         bytes calldata walletPublicKey,
         bytes calldata preimage,
@@ -441,24 +363,9 @@ contract MaintainerProxy is Ownable, Reimbursable {
         );
     }
 
-    /// @notice Wraps defeat fraud challenge with heartbeat which allows to
-    ///         defeat a pending fraud challenge against a wallet by proving the
-    ///         sighash and signature were produced for an off-chain wallet
-    ///         heartbeat message following a strict format.
-    ///         In order to defeat the challenge the same `walletPublicKey` and
-    ///         signature (represented by `r`, `s` and `v`) must be provided as
-    ///         were used to calculate the sighash during heartbeat message
-    ///         signing. The fraud challenge defeat attempt will only succeed if
-    ///         the signed message follows a strict format required for
-    ///         heartbeat messages. If successfully defeated, the fraud
-    ///         challenge is marked as resolved and the amount of ether
-    ///         deposited by the challenger is sent to the treasury.
-    /// @param walletPublicKey The public key of the wallet in the uncompressed
-    ///        and unprefixed format (64 bytes)
-    /// @param heartbeatMessage Off-chain heartbeat message meeting the heartbeat
-    ///        message format requirements which produces sighash used to
-    ///        generate the ECDSA signature that is the subject of the fraud
-    ///        claim
+    /// @notice Wraps `Bridge.defeatFraudChallengeWithHeartbeat` call and
+    ///         reimburses the caller's transaction cost.
+    /// @dev See `Bridge.defeatFraudChallengeWithHeartbeat` function documentation.
     function defeatFraudChallengeWithHeartbeat(
         bytes calldata walletPublicKey,
         bytes calldata heartbeatMessage
@@ -535,13 +442,13 @@ contract MaintainerProxy is Ownable, Reimbursable {
     /// @notice Updates the values of gas offset parameters.
     /// @dev Can be called only by the contract owner. The caller is responsible
     ///      for validating parameters.
-    /// @param submitDepositSweepProofGasOffset New request wallet gas offset
+    /// @param submitDepositSweepProofGasOffset New submit deposit sweep proof gas offset
     /// @param submitRedemptionProofGasOffset New submit redemption proof gas offset
     /// @param submitMovingFundsCommitmentGasOffset New submit moving funds commitment gas offset
     /// @param submitMovingFundsProofGasOffset New submit moving funds proof gas offset
-    /// @param notifyMovingFundsBelowDustGasOffset New notification moving funds below dust gas offset
+    /// @param notifyMovingFundsBelowDustGasOffset New notify moving funds below dust gas offset
     /// @param submitMovedFundsSweepProofGasOffset New submit moved funds sweep proof gas offset
-    /// @param requestNewWalletGasOffset New request wallet gas offset
+    /// @param requestNewWalletGasOffset New request new wallet gas offset
     /// @param notifyWalletCloseableGasOffset New notify closeable wallet gas offset
     /// @param notifyWalletClosingPeriodElapsedGasOffset New notify wallet closing period elapsed gas offset
     /// @param defeatFraudChallengeGasOffset New defeat fraud challenge gas offset
