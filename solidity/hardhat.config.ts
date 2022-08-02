585ee30d4ccf43ea545057d4fff5686c387b2788
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2022-08-02 11:33:38 +0200

    Add `BridgeStub` as contract sizer exception
    There is no more room for easy size reduction
    in the `BridgeStub` contract. The only simple
    way to overcome that problem is adding that
    contract as an exception to the contract
    sizer plugin.

--- solidity/hardhat.config.ts
+++ solidity/hardhat.config.ts
@@ -198,6 +198,7 @@ const config: HardhatUserConfig = {
     disambiguatePaths: false,
     runOnCompile: true,
     strict: true,
+    except: ["BridgeStub$"],
   },
   mocha: {
     timeout: 60_000,
