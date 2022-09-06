48b54c8bebdaca20defc8d6c986c8102ead443a6
Piotr Dyraga <piotr.dyraga@keep.network>
2022-09-06 14:45:29 +0200

    Be more specific about year 2106 problem and unit32

--- solidity/contracts/bridge/Redemption.sol
+++ solidity/contracts/bridge/Redemption.sol
@@ -145,7 +145,7 @@ library Redemption {
         // creation.
         uint64 txMaxFee;
         // UNIX timestamp the request was created at.
-        // XXX: unsigned 32-bit integer unix seconds, will break around 2106
+        // XXX: Unsigned 32-bit int unix seconds, will break February 7th 2106.
         uint32 requestedAt;
         // This struct doesn't contain `__gap` property as the structure is stored
         // in a mapping, mappings store values in different slots and they are
