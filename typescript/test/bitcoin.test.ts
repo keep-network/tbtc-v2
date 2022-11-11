d490dcd4d839243b4f020892cf3a2cf57dfe11f3
michalsmiarowski <michal.smiarowski@thesis.co>
2022-11-11 11:31:51 +0100

    Add tests
    Adds tests that checks if the `generateP2PKHAddress` and `decodeP2PKHAddress`
    functions work properly.

--- typescript/test/bitcoin.test.ts
+++ typescript/test/bitcoin.test.ts
@@ -1,5 +1,5 @@
 import { expect } from "chai"
-import { compressPublicKey } from "../src/bitcoin"
+import { compressPublicKey, decodeP2PKHAddress, generateP2PKHAddress } from "../src/bitcoin"
 
 describe("Bitcoin", () => {
   describe("compressPublicKey", () => {
@@ -45,4 +45,68 @@ describe("Bitcoin", () => {
       })
     })
   })
+
+  describe("generateP2PKHAddress", () => {
+    context("when compressed public key is provided", () => {
+      it("should generate proper P2PKH address", () => {
+        const compressedPublicKey =
+          "02ff6e1857db52d6dba2bd4239fba722655622bc520709d38011f9adac8ea3477b"
+
+        const bitcoinAddress =
+          "6pyyJxc6x6Ev8utcxUYVqqNjSrSjdixNJv9WnoXJEHtBpPGa5m"
+
+        expect(generateP2PKHAddress(compressedPublicKey)).to.be.equal(
+          bitcoinAddress
+        )
+      })
+    })
+
+    context("when uncompressed public key is provided", () => {
+      it("should throw", () => {
+        const uncompressedPublicKey =
+          "ff6e1857db52d6dba2bd4239fba722655622bc520709d38011f9adac8ea3477b" +
+          "45ae275b657f7bac7c1e3d146a564051aee1356895f01e4f29f333502416fa4a"
+
+        expect(() => generateP2PKHAddress(uncompressedPublicKey)).to.throw(
+          "Public key must be compressed"
+        )
+      })
+    })
+  })
+
+  describe("decodeP2PKHAddress", () => {
+    context("when proper P2PKH address is provided", () => {
+      it("should return proper compressed public key", () => {
+        const compressedPublicKey =
+          "02ff6e1857db52d6dba2bd4239fba722655622bc520709d38011f9adac8ea3477b"
+
+        const bitcoinAddress =
+          "6pyyJxc6x6Ev8utcxUYVqqNjSrSjdixNJv9WnoXJEHtBpPGa5m"
+
+        expect(decodeP2PKHAddress(bitcoinAddress)).to.be.equal(
+          compressedPublicKey
+        )
+      })
+
+      it("should return the same public key that was used in `generateP2PKHAddress` function", () => {
+        const compressedPublicKey =
+            "03474444cca71c678f5019d16782b6522735717a94602085b4adf707b465c36ca8"
+
+        const bitcoinAddress = generateP2PKHAddress(compressedPublicKey)
+
+        expect(decodeP2PKHAddress(bitcoinAddress)).to.be.equal(compressedPublicKey)
+      })
+    })
+
+    context("when random string is provided as a P2PKH address", () => {
+      it("should throw", () => {
+        const bitcoinAddress = "random string"
+
+        expect(() => decodeP2PKHAddress(bitcoinAddress)).to.throw(
+          "Decoding failed."
+        )
+      })
+    })
+  })
+
 })
