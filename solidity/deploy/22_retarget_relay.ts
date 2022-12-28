e7c3ccd258d1fed1ad5ea0375f99cbff4caa2bd5
Tomasz Slabon <tomasz.slabon@keep.network>
2022-12-28 14:10:39 +0100

    Added LightRelay retarget script

--- solidity/deploy/22_retarget_relay.ts
+++ solidity/deploy/22_retarget_relay.ts
@@ -1,1 +1,23 @@
-// TODO: Implement the first relay retarget
+import { HardhatRuntimeEnvironment } from "hardhat/types"
+import { DeployFunction } from "hardhat-deploy/types"
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { getNamedAccounts, deployments } = hre
+  const { execute } = deployments
+  const { deployer } = await getNamedAccounts()
+
+  // TODO: Fill with proper value
+  const headers = "0x000000000000000000000000000000000000000000000000"
+
+  await execute(
+    "LightRelay",
+    { from: deployer, log: true, waitConfirmations: 1 },
+    "retarget",
+    headers
+  )
+}
+
+export default func
+
+func.tags = ["RetargetLightRelay"]
+func.dependencies = ["LightRelay"]
