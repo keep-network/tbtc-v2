46ebca8d6697daafff0f535f9e93a184b5d5842e
Tomasz Slabon <tomasz.slabon@keep.network>
2022-12-28 14:17:18 +0100

    Added LightRelay maintainer authorization

--- solidity/deploy/23_authorize_relay_maintainer.ts
+++ solidity/deploy/23_authorize_relay_maintainer.ts
@@ -1,2 +1,27 @@
-// TODO: Implement setting of authorization requirement and set some maintainer
-//       address as authorized
+import { HardhatRuntimeEnvironment } from "hardhat/types"
+import { DeployFunction } from "hardhat-deploy/types"
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { getNamedAccounts, deployments } = hre
+  const { execute } = deployments
+  const { deployer } = await getNamedAccounts()
+
+  await execute(
+    "LightRelay",
+    { from: deployer, log: true, waitConfirmations: 1 },
+    "setAuthorizationStatus",
+    true
+  )
+
+  await execute(
+    "LightRelay",
+    { from: deployer, log: true, waitConfirmations: 1 },
+    "authorize",
+    deployer
+  )
+}
+
+export default func
+
+func.tags = ["AuthorizeLightRelayMaintainer"]
+func.dependencies = ["LightRelay"]
