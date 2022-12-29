260d99f94061920db80a4cc547a9360afd2dd465
Jakub Nowakowski <jakub.nowakowski@akena.co>
2022-12-29 12:48:17 +0100

    Transfer VendingMachine roles only if needed
    Before executing roles transfer the script checks if the roles are
    already set to the desired addresses.
    This is to support execution of the deployment after it was already
    executed.

--- solidity/deploy/03_transfer_vending_machine_roles.ts
+++ solidity/deploy/03_transfer_vending_machine_roles.ts
@@ -3,31 +3,40 @@ import { DeployFunction } from "hardhat-deploy/types"
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { getNamedAccounts, helpers, deployments } = hre
-  const { execute, log } = deployments
+  const { execute, read, log } = deployments
   const { deployer, keepTechnicalWalletTeam, keepCommunityMultiSig } =
     await getNamedAccounts()
 
-  log(
-    `transferring vendingMachineUpgradeInitiator role to ${keepTechnicalWalletTeam}`
-  )
+  const vendingMachineOwner: string = await read("VendingMachine", "owner")
+  if (vendingMachineOwner === keepTechnicalWalletTeam) {
+    log(
+      `transferring vendingMachineUpgradeInitiator role to ${keepTechnicalWalletTeam}`
+    )
 
-  await execute(
+    await execute(
+      "VendingMachine",
+      { from: deployer, log: true, waitConfirmations: 1 },
+      "transferVendingMachineUpgradeInitiatorRole",
+      keepTechnicalWalletTeam
+    )
+  }
+
+  const unmintFeeUpdateInitiator: string = await read(
     "VendingMachine",
-    { from: deployer, log: true, waitConfirmations: 1 },
-    "transferVendingMachineUpgradeInitiatorRole",
-    keepTechnicalWalletTeam
+    "unmintFeeUpdateInitiator"
   )
+  if (unmintFeeUpdateInitiator === keepTechnicalWalletTeam) {
+    log(
+      `transferring unmintFeeUpdateInitiator role to ${keepTechnicalWalletTeam}`
+    )
 
-  log(
-    `transferring unmintFeeUpdateInitiator role to ${keepTechnicalWalletTeam}`
-  )
-
-  await execute(
-    "VendingMachine",
-    { from: deployer, log: true, waitConfirmations: 1 },
-    "transferUnmintFeeUpdateInitiatorRole",
-    keepTechnicalWalletTeam
-  )
+    await execute(
+      "VendingMachine",
+      { from: deployer, log: true, waitConfirmations: 1 },
+      "transferUnmintFeeUpdateInitiatorRole",
+      keepTechnicalWalletTeam
+    )
+  }
 
   await helpers.ownable.transferOwnership(
     "VendingMachine",
