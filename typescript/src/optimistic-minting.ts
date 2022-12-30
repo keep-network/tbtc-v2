150a650ef6c56172071d31a617b01791e5b22d6b
Jakub Nowakowski <jakub.nowakowski@akena.co>
2022-12-30 14:55:14 +0100

    Add function to query OptimisticMintingRequested events

--- typescript/src/optimistic-minting.ts
+++ typescript/src/optimistic-minting.ts
@@ -1,3 +1,16 @@
+import { BigNumber } from "ethers"
+import { TransactionHash } from "./bitcoin"
+import { Identifier, Event } from "./chain"
+
+export type OptimisticMintingRequestedEvent = {
+  minter: Identifier
+  depositKey: BigNumber
+  depositor: Identifier
+  amount: BigNumber
+  fundingTxHash: TransactionHash
+  fundingOutputIndex: number
+} & Event
+
 export type OptimisticMintingRequest = {
   requestedAt: number
   finalizedAt: number
