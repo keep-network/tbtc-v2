e652e7e61f070fa573232761b15a87adddb5adc8
Tomasz Slabon <tomasz.slabon@keep.network>
2023-01-04 11:44:21 +0100

    Fixed script deployment dependencies

--- solidity/deploy/22_retarget_relay.ts
+++ solidity/deploy/22_retarget_relay.ts
@@ -112,4 +112,4 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 export default func
 
 func.tags = ["RetargetLightRelay"]
-func.dependencies = ["LightRelay"]
+func.dependencies = ["GenesisLightRelay"]
--- solidity/deploy/23_authorize_relay_maintainer.ts
+++ solidity/deploy/23_authorize_relay_maintainer.ts
@@ -17,4 +17,4 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 export default func
 
 func.tags = ["AuthorizeLightRelayMaintainer"]
-func.dependencies = ["LightRelay"]
+func.dependencies = ["RetargetLightRelay"]
