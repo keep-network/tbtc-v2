e3e57f3aa43c80c17e5df35f5ba1d09873e469ae
Dmitry <dymitr.paremski@keep.network>
2023-01-09 23:10:25 +0100

    Including localhost as local (hardhat) network
    Some commands require to use hardhat as a local network, but in some
    places in the code we also use localhost. We need to include both.

--- solidity/deploy/00_resolve_relay.ts
+++ solidity/deploy/00_resolve_relay.ts
@@ -21,10 +21,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     // Temporarily for system tests, that run on `localhost` network we deploy
     // a `TestRelay` contract, until the relay maintainer is ready.
     await deployments.deploy("LightRelay", {
-      contract:
-        hre.network.name === "goerli" || hre.network.name === "localhost"
-          ? "TestRelay"
-          : "LightRelay",
+      contract: hre.network.name !== "mainnet" ? "TestRelay" : "LightRelay",
       from: deployer,
       log: true,
       waitConfirmations: 1,
