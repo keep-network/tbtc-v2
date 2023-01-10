0056ca2d16956bfd15319a5c678633ef40b66111
Tomasz Slabon <tomasz.slabon@keep.network>
2023-01-10 15:17:02 +0100

    Added explanatory comment

--- solidity/contracts/test/GoerliLightRelay.sol
+++ solidity/contracts/test/GoerliLightRelay.sol
@@ -18,15 +18,17 @@ pragma solidity 0.8.17;
 import "../relay/LightRelay.sol";
 
 /// @title Goerli Light Relay
-/// @notice GoerliLightRelay is a stub version of `LightRelay` intended to be
+/// @notice GoerliLightRelay is a stub version of LightRelay intended to be
 ///         used on the Goerli test network. It always returns `1` as the current
 ///         and previous difficulties making it possible to bypass validation of
 ///         difficulties of Bitcoin testnet blocks. Since difficulty in Bitcoin
 ///         testnet often falls to `1` it would not be possible to validate
-///         blocks with the real `LightRelay`.
+///         blocks with the real LightRelay.
 /// @dev Since the returned difficulties are fixed and impossible to be modified
 ///      with any setters, it is safe to use GoerliLightRelay without risking
 ///      somebody may change the difficulties and block the test network.
+///      Notice that GoerliLightRelay is derived from LightRelay so that the two
+///      contracts have the same API and correct bindings can be generated.
 contract GoerliLightRelay is LightRelay {
     /// @notice Returns `1` as the difficulty of the previous epoch.
     function getPrevEpochDifficulty() external view override returns (uint256) {
