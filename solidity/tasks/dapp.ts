93773eb4a56944565cd39791f976129348692741
michalsmiarowski <michal.smiarowski@thesis.co>
2023-01-12 09:49:13 +0100

    Add optimistic minting tasks
    Add tasks with which you will be able to do an optimistic minting.

--- solidity/tasks/dapp.ts
+++ solidity/tasks/dapp.ts
@@ -3,7 +3,7 @@
 
 import { task, types } from "hardhat/config"
 import type { HardhatRuntimeEnvironment } from "hardhat/types"
-import type { Bridge } from "../typechain"
+import type { Bridge, TBTCVault } from "../typechain"
 
 interface UTXO {
   txHash: string
@@ -104,6 +104,67 @@ task(
   .setAction(async (args, hre) => {
     const { redeemerAddress } = args
     await getRedemptions(hre, redeemerAddress)
+  })
+
+task("dapp:set-vault-status", "Sets vault status")
+  .addParam("address", "Address of the vault.", undefined, types.string)
+  .addParam(
+    "isTrusted",
+    "Sets the vault status as trusted if true, otherwise it's not trusted",
+    undefined,
+    types.boolean
+  )
+  .setAction(async (args, hre) => {
+    const { address, isTrusted } = args
+    await setVaultStatus(hre, address, isTrusted)
+  })
+
+task("dapp:add-minter", "Adds new minter")
+  .addParam(
+    "address",
+    "Address that will be set as minter.",
+    undefined,
+    types.string
+  )
+  .setAction(async (args, hre) => {
+    const { address } = args
+    await addMinter(hre, address)
+  })
+
+task("dapp:request-optimistic-minting", "Requests optimistc minting")
+  .addParam(
+    "fundingTxHash",
+    "Funding transaction hash.",
+    undefined,
+    types.string
+  )
+  .addParam(
+    "fundingOutputIndex",
+    "Index of the funding output belonging to the funding transaction. Emitted in the `DepositRevealed` event.",
+    undefined,
+    types.int
+  )
+  .setAction(async (args, hre) => {
+    const { fundingTxHash, fundingOutputIndex } = args
+    await requestOptimisticMint(hre, fundingTxHash, fundingOutputIndex)
+  })
+
+task("dapp:finalize-optimistic-minting", "Finalizes optimistc minting")
+  .addParam(
+    "fundingTxHash",
+    "Funding transaction hash.",
+    undefined,
+    types.string
+  )
+  .addParam(
+    "fundingOutputIndex",
+    "Index of the funding output belonging to the funding transaction. Emitted in the `DepositRevealed` event.",
+    undefined,
+    types.int
+  )
+  .setAction(async (args, hre) => {
+    const { fundingTxHash, fundingOutputIndex } = args
+    await finalizeOptimisticMint(hre, fundingTxHash, fundingOutputIndex)
   })
 
 async function registerWallet(hre: HardhatRuntimeEnvironment, utxo: UTXO) {
@@ -224,4 +285,77 @@ async function getRedemptions(
     ...event.args,
   }))
   console.table(events, ["walletPubKeyHash", "redeemerOutputScript"])
+}
+
+async function setVaultStatus(
+  hre: HardhatRuntimeEnvironment,
+  address: string,
+  isTrusted: boolean
+) {
+  const { getNamedAccounts, deployments, helpers } = hre
+  const { execute } = deployments
+  const { deployer } = await getNamedAccounts()
+
+  await execute(
+    "BridgeGovernance",
+    { from: deployer, log: true, waitConfirmations: 1 },
+    "setVaultStatus",
+    address,
+    isTrusted
+  )
+
+  console.log(`Vault (${address}) is now ${!isTrusted ? "NOT" : ""} trusted!`)
+}
+
+async function addMinter(hre: HardhatRuntimeEnvironment, address: string) {
+  const { getNamedAccounts, deployments, helpers } = hre
+  const { execute } = deployments
+  const { deployer } = await getNamedAccounts()
+
+  await execute(
+    "TBTCVault",
+    { from: deployer, log: true, waitConfirmations: 1 },
+    "addMinter",
+    address
+  )
+
+  console.log(`Successfully added minter: ${address}!`)
+}
+
+async function requestOptimisticMint(
+  hre: HardhatRuntimeEnvironment,
+  fundingTxHash: string,
+  fundingOutputIndex: number
+) {
+  const { helpers } = hre
+  const tbtcVault = await helpers.contracts.getContract<TBTCVault>("TBTCVault")
+
+  const tx = await tbtcVault.requestOptimisticMint(
+    fundingTxHash,
+    fundingOutputIndex
+  )
+  await tx.wait()
+
+  console.log(
+    `Optimistic Minting requested successfuly for a deposit with tx hash ${fundingTxHash}!`
+  )
+}
+
+async function finalizeOptimisticMint(
+  hre: HardhatRuntimeEnvironment,
+  fundingTxHash: string,
+  fundingOutputIndex: number
+) {
+  const { helpers, ethers } = hre
+  const tbtcVault = await helpers.contracts.getContract<TBTCVault>("TBTCVault")
+
+  const tx = await tbtcVault.finalizeOptimisticMint(
+    fundingTxHash,
+    fundingOutputIndex
+  )
+  await tx.wait()
+
+  console.log(
+    `Finalized Optimistic Minting for a deposit with tx hash ${fundingTxHash}!`
+  )
 }
