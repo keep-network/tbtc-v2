73a63ad0f35b6fb1e6b10bea3ee4f519d1eb25ab
Jakub Nowakowski <jakub.nowakowski@akena.co>
2023-01-20 22:04:00 +0100

    Add equals function to Identifier interface
    Function to comapre two identifiers.

--- typescript/src/chain.ts
+++ typescript/src/chain.ts
@@ -17,6 +17,12 @@ export interface Identifier {
    * Identifier as an un-prefixed hex string.
    */
   identifierHex: string
+  /**
+   * Checks if two identifiers are equal.
+   *
+   * @param identifier Another identifier
+   */
+  equals(identifier: Identifier): boolean
 }
 
 /**
