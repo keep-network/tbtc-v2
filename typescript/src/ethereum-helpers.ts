5e19011be61bf8b2009e3d67ad4765bc37440a8e
Jakub Nowakowski <jakub.nowakowski@akena.co>
2023-02-02 21:58:08 +0100

    Resolve revert messages from errors thrown on transaction send
    Ethers returns errors in a complex, hard to read structure (example
    below). Here we try to resolve the actual revert message from the
    structure and return it as a single string of the error message.
    
    ```
    {
        "reason": "cannot estimate gas; transaction may fail or may require manual gas limit",
        "code": "UNPREDICTABLE_GAS_LIMIT",
        "error":
        {
            "reason": "execution reverted: Optimistic minting already finalized for the deposit",
            "code": "UNPREDICTABLE_GAS_LIMIT",
            "method": "estimateGas",
            "transaction":
            {
                "from": "0x2c6651c457f7734fA3b011fFa218eD4A2f1aa45a",
                "maxPriorityFeePerGas":
                {
                    "type": "BigNumber",
                    "hex": "0x59682f00"
                },
                "maxFeePerGas":
                {
                    "type": "BigNumber",
                    "hex": "0x1bc8c2ab38"
                },
                "to": "0x65eB0562FCe858f8328858c76E689aBedB78621F",
                "data": "0x6abe3a6c437db270c650c8b3fc88903626c8c32caa363917d676060486fa50f522eba74d0000000000000000000000000000000000000000000000000000000000000000",
                "type": 2,
                "accessList": null
            },
            "error":
            {
                "reason": "processing response error",
                "code": "SERVER_ERROR",
                "body": "{\"jsonrpc\":\"2.0\",\"id\":59,\"error\":{\"code\":3,\"message\":\"execution reverted: Optimistic minting already finalized for the deposit\",\"data\":\"0x08c379a0000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000000344f7074696d6973746963206d696e74696e6720616c72656164792066696e616c697a656420666f7220746865206465706f736974000000000000000000000000\"}}",
                "error":
                {
                    "code": 3,
                    "data": "0x08c379a0000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000000344f7074696d6973746963206d696e74696e6720616c72656164792066696e616c697a656420666f7220746865206465706f736974000000000000000000000000"
                },
                "requestBody": "{\"method\":\"eth_estimateGas\",\"params\":[{\"type\":\"0x2\",\"maxFeePerGas\":\"0x1bc8c2ab38\",\"maxPriorityFeePerGas\":\"0x59682f00\",\"from\":\"0x2c6651c457f7734fa3b011ffa218ed4a2f1aa45a\",\"to\":\"0x65eb0562fce858f8328858c76e689abedb78621f\",\"data\":\"0x6abe3a6c437db270c650c8b3fc88903626c8c32caa363917d676060486fa50f522eba74d0000000000000000000000000000000000000000000000000000000000000000\"}],\"id\":59,\"jsonrpc\":\"2.0\"}",
                "requestMethod": "POST",
                "url": "https://eth-goerli.g.alchemy.com/v2/Nxtaz-328NCYIX2JY7LTubwX7A76JbyF"
            }
        },
        "tx":
        {
            "data": "0x6abe3a6c437db270c650c8b3fc88903626c8c32caa363917d676060486fa50f522eba74d0000000000000000000000000000000000000000000000000000000000000000",
            "to":
            {},
            "from": "0x2c6651c457f7734fA3b011fFa218eD4A2f1aa45a",
            "type": 2,
            "maxFeePerGas":
            {
                "type": "BigNumber",
                "hex": "0x1bc8c2ab38"
            },
            "maxPriorityFeePerGas":
            {
                "type": "BigNumber",
                "hex": "0x59682f00"
            },
            "nonce":
            {},
            "gasLimit":
            {},
            "chainId":
            {}
        }
    }
    ```

--- typescript/src/ethereum-helpers.ts
+++ typescript/src/ethereum-helpers.ts
@@ -12,7 +12,9 @@ import {
  * @param nonRetryableErrors List of error messages that if returned from executed
  *        function, should break the retry loop and return immediately.
  * @returns Result of function execution.
- * @throws An error returned by function execution.
+ * @throws An error returned by function execution. An error thrown by the executed
+ *         function is processed by {@link resolveEthersError} function to resolve
+ *         the revert message in case of a transaction revert.
  */
 export async function sendWithRetry<T>(
   fn: () => Promise<T>,
@@ -28,7 +30,44 @@ export async function sendWithRetry<T>(
     try {
       return await fn()
     } catch (err: unknown) {
-      throw err
+      throw resolveEthersError(err)
     }
   })
+}
+
+/**
+ * Represents an interface that matches the errors structure thrown by ethers library.
+ * {@see {@link https://github.com/ethers-io/ethers.js/blob/c80fcddf50a9023486e9f9acb1848aba4c19f7b6/packages/logger/src.ts/index.ts#L268-L277 ethers source code}}
+ */
+interface EthersError extends Error {
+  reason: string
+  code: string
+  error: unknown
+}
+
+/**
+ * Takes an error and tries to resolve a revert message if the error is related
+ * to reverted transaction.
+ * @param err Error to process.
+ * @returns Error with a revert message or the input error when the error could
+ *          not be resolved successfully.
+ */
+function resolveEthersError(err: unknown): unknown {
+  const isEthersError = (obj: any): obj is EthersError => {
+    return "reason" in obj && "code" in obj && "error" in obj
+  }
+
+  if (isEthersError(err) && err !== null) {
+    // Ethers errors are nested. The parent UNPREDICTABLE_GAS_LIMIT has a general
+    // reason "cannot estimate gas; transaction may fail or may require manual gas limit",
+    if (err.code === "UNPREDICTABLE_GAS_LIMIT") {
+      if (typeof isEthersError(err["error"]) && err["error"] !== null) {
+        // The nested error is expected to contain a reason property with a message
+        // of the transaction revert.
+        return new Error((err["error"] as EthersError).reason)
+      }
+    }
+  }
+
+  return err
 }
