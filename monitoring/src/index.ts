fdd0fa61855eb9afd27686a0f56abb0c51786e16
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2023-02-06 12:05:22 +0100

    Rename entrypoint

--- monitoring/src/index.ts
+++ monitoring/src/index.ts
@@ -1,5 +1,5 @@
 import * as ff from '@google-cloud/functions-framework';
 
-ff.http('monitoring', (request: ff.Request, response: ff.Response) => {
+ff.http('check', (request: ff.Request, response: ff.Response) => {
   response.send('OK');
 });
