715fbde0493fd532f6aa8147e7431bf52a97af61
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2023-02-08 17:37:00 +0100

    Wire up the incident pipeline and expose it using the Google Functions Framework

--- monitoring/src/index.ts
+++ monitoring/src/index.ts
@@ -1,5 +1,33 @@
-import * as ff from '@google-cloud/functions-framework';
+import * as ff from '@google-cloud/functions-framework'
+import {
+  Monitor as IncidentMonitor,
+  Receiver as IncidentReceiver,
+  Manager as IncidentManager
+} from "./incident"
+import {DepositMonitor} from "./deposit-monitor"
+import {contracts} from "./contracts"
+import {DiscordReceiver} from "./discord-receiver"
+import {SentryReceiver} from "./sentry-receiver"
 
-ff.http('check', (request: ff.Request, response: ff.Response) => {
-  response.send('OK');
-});
+const incidentMonitors: IncidentMonitor[] = [
+  new DepositMonitor(contracts.Bridge)
+]
+
+const incidentReceivers: IncidentReceiver[] = [
+  new DiscordReceiver(),
+  new SentryReceiver()
+]
+
+const incidentManager = new IncidentManager(
+  incidentMonitors,
+  incidentReceivers
+)
+
+ff.http('check', async (request: ff.Request, response: ff.Response) => {
+  try {
+    await incidentManager.check()
+    response.status(200)
+  } catch (err) {
+    response.status(500).send(err)
+  }
+})
