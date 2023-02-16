05d95c0a9a93c5adcf6053a347bf353da931ec8b
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2023-02-16 11:51:25 +0100

    Implement wallet monitor

--- monitoring/src/wallet-monitor.ts
+++ monitoring/src/wallet-monitor.ts
@@ -1,11 +1,35 @@
+import { SystemEventType } from "./system-event"
+
+import type { NewWalletRegisteredEvent as WalletRegisteredChainEvent } from "@keep-network/tbtc-v2.ts/dist/src/wallet"
+import type { Bridge } from "@keep-network/tbtc-v2.ts/dist/src/chain"
 import type { SystemEvent, Monitor as SystemEventMonitor } from "./system-event"
 
+const WalletRegistered = (
+  chainEvent: WalletRegisteredChainEvent
+): SystemEvent => ({
+  title: "Wallet registered",
+  type: SystemEventType.Informational,
+  data: {
+    ecdsaWalletID: chainEvent.ecdsaWalletID.toString(),
+    walletPublicKeyHash: chainEvent.walletPublicKeyHash.toString(),
+    ethRegistrationTxHash: chainEvent.transactionHash.toPrefixedString(),
+  },
+  block: chainEvent.blockNumber,
+})
+
 export class WalletMonitor implements SystemEventMonitor {
+  private bridge: Bridge
+
+  constructor(bridge: Bridge) {
+    this.bridge = bridge
+  }
+
   async check(fromBlock: number, toBlock: number): Promise<SystemEvent[]> {
-    // TODO: Implement the wallet monitor.
-    console.log(
-      `checked stub wallet monitor from block ${fromBlock} to block ${toBlock}`
-    )
-    return []
+    const chainEvents = await this.bridge.getNewWalletRegisteredEvents({
+      fromBlock,
+      toBlock,
+    })
+
+    return chainEvents.map(WalletRegistered)
   }
 }
