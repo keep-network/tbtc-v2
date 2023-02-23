660c47b1b4c1ff77d95c93724f09a567f2a9f24f
Piotr Dyraga <piotr.dyraga@keep.network>
2023-02-23 10:49:09 +0100

    Mint and burn functionality for L2TBTC token

--- solidity/contracts/l2/L2TBTC.sol
+++ solidity/contracts/l2/L2TBTC.sol
@@ -15,19 +15,21 @@
 
 pragma solidity ^0.8.17;
 
-// EIP-2612 is Final as of 2022-11-01. This file is deprecated in the `main`
-// branch of @openzeppelin/contracts-upgradeable. Once a version > 4.8.1 gets
-// released, we should drop the `draft-*` prefix from this import.
 import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/draft-ERC20PermitUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
 // TODO:
 // * Be paused by any one of `n` guardians, allowing avoidance of contagion in case
 //  of a chain- or bridge-specific incident.
-// * Mint by minter.
 // * Proper documentation.
-// * Test initialization
-contract L2TBTC is ERC20PermitUpgradeable, OwnableUpgradeable {
+// * Proper tests, including initialization.
+contract L2TBTC is
+    ERC20Upgradeable,
+    ERC20BurnableUpgradeable,
+    ERC20PermitUpgradeable,
+    OwnableUpgradeable
+{
     /// @notice Indicates if the given address is a Minter. Only Minters can
     ///         mint the token.
     mapping(address => bool) public isMinter;
@@ -47,12 +49,30 @@ contract L2TBTC is ERC20PermitUpgradeable, OwnableUpgradeable {
         external
         initializer
     {
-        __Ownable_init();
+        // OpenZeppelin upgradeable contracts documentation says:
+        //
+        // "Use with multiple inheritance requires special care. Initializer
+        // functions are not linearized by the compiler like constructors.
+        // Because of this, each __{ContractName}_init function embeds the
+        // linearized calls to all parent initializers. As a consequence,
+        // calling two of these init functions can potentially initialize the
+        // same contract twice."
+        //
+        // Note that ERC20 extensions do not linearize calls to ERC20Upgradeable
+        // initializer so we call all extension initializers individually. At
+        // the same time, ERC20PermitUpgradeable does linearize the call to
+        // EIP712Upgradeable so we are not using the unchained initializer
+        // versions.
         __ERC20_init(_name, _symbol);
+        __ERC20Burnable_init();
         __ERC20Permit_init(_name);
+        __Ownable_init();
     }
 
     /// @notice Adds the address to the Minter list.
+    /// @dev Requirements:
+    ///      - The caller must be the contract owner.
+    ///      - `minter` must not be a minter address.
     function addMinter(address minter) external onlyOwner {
         require(!isMinter[minter], "This address is already a minter");
         isMinter[minter] = true;
@@ -61,6 +81,9 @@ contract L2TBTC is ERC20PermitUpgradeable, OwnableUpgradeable {
     }
 
     /// @notice Removes the address from the Minter list.
+    /// @dev Requirements:
+    ///      - The caller must be the contract owner.
+    ///      - `minter` must be a minter address.
     function removeMinter(address minter) external onlyOwner {
         require(isMinter[minter], "This address is not a minter");
         delete isMinter[minter];
@@ -76,6 +99,16 @@ contract L2TBTC is ERC20PermitUpgradeable, OwnableUpgradeable {
         }
 
         emit MinterRemoved(minter);
+    }
+
+    /// @notice Allows one of the minters to mint `amount` tokens and assign
+    ///         them to `account`, increasing the total supply. Emits
+    ///         a `Transfer` event with `from` set to the zero address.
+    /// @dev Requirements:
+    ///      - The caller must be a minter.
+    ///      - `account` must not be the zero address.
+    function mint(address account, uint256 amount) external onlyMinter {
+        _mint(account, amount);
     }
 
     /// @notice Allows to fetch a list of all Minters.
--- solidity/test/l2/L2TBTC.test.ts
+++ solidity/test/l2/L2TBTC.test.ts
@@ -1,12 +1,16 @@
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers"
-import { randomBytes } from "crypto"
+import { randomBytes, Sign } from "crypto"
 import { ethers, getUnnamedAccounts, helpers, waffle } from "hardhat"
 import { expect } from "chai"
 import { ContractTransaction } from "ethers"
+import { BigNumber } from "@ethersproject/bignumber"
+import { to1e18 } from "../helpers/contract-test-helpers"
 
 import type { L2TBTC } from "../../typechain"
 
 const { createSnapshot, restoreSnapshot } = helpers.snapshot
+
+const ZERO_ADDRESS = ethers.constants.AddressZero
 
 describe("L2TBTC", () => {
   const fixture = async () => {
@@ -15,6 +19,7 @@ describe("L2TBTC", () => {
     const accounts = await getUnnamedAccounts()
     const minter = await ethers.getSigner(accounts[1])
     const thirdParty = await ethers.getSigner(accounts[2])
+    const tokenHolder = await ethers.getSigner(accounts[3])
 
     const deployment = await helpers.upgrades.deployProxy(
       // Hacky workaround allowing to deploy proxy contract any number of times
@@ -38,6 +43,7 @@ describe("L2TBTC", () => {
       governance,
       minter,
       thirdParty,
+      tokenHolder,
       token,
     }
   }
@@ -47,12 +53,12 @@ describe("L2TBTC", () => {
   let governance: SignerWithAddress
   let minter: SignerWithAddress
   let thirdParty: SignerWithAddress
+  let tokenHolder: SignerWithAddress
 
   before(async () => {
     // eslint-disable-next-line @typescript-eslint/no-extra-semi
-    ;({ governance, minter, thirdParty, token } = await waffle.loadFixture(
-      fixture
-    ))
+    ;({ governance, minter, thirdParty, tokenHolder, token } =
+      await waffle.loadFixture(fixture))
   })
 
   describe("addMinter", () => {
@@ -257,5 +263,213 @@ describe("L2TBTC", () => {
         })
       })
     })
+  })
+
+  describe("mint", () => {
+    const amount = to1e18(50)
+
+    before(async () => {
+      await createSnapshot()
+      await token.connect(governance).addMinter(minter.address)
+    })
+
+    after(async () => {
+      await restoreSnapshot()
+    })
+
+    context("when called not by a minter", () => {
+      it("should revert", async () => {
+        await expect(
+          token.connect(thirdParty).mint(thirdParty.address, amount)
+        ).to.be.revertedWith("Caller is not a minter")
+      })
+    })
+
+    context("when called by a minter", () => {
+      context("for a zero account", () => {
+        it("should revert", async () => {
+          await expect(
+            token.connect(minter).mint(ZERO_ADDRESS, amount)
+          ).to.be.revertedWith("ERC20: mint to the zero address")
+        })
+      })
+
+      context("for a non-zero account", () => {
+        let mintTx: ContractTransaction
+
+        before(async () => {
+          await createSnapshot()
+          mintTx = await token.connect(minter).mint(tokenHolder.address, amount)
+        })
+
+        after(async () => {
+          await restoreSnapshot()
+        })
+
+        it("should increment totalSupply", async () => {
+          expect(await token.totalSupply()).to.equal(amount)
+        })
+
+        it("should increment recipient balance", async () => {
+          expect(await token.balanceOf(tokenHolder.address)).to.equal(amount)
+        })
+
+        it("should emit Transfer event", async () => {
+          await expect(mintTx)
+            .to.emit(token, "Transfer")
+            .withArgs(ZERO_ADDRESS, tokenHolder.address, amount)
+        })
+      })
+    })
+  })
+
+  describe("burn", () => {
+    const initialSupply = to1e18(36)
+    const initialBalance = to1e18(18)
+
+    before(async () => {
+      await createSnapshot()
+      await token.connect(governance).addMinter(minter.address)
+      await token.connect(minter).mint(tokenHolder.address, initialBalance)
+      await token.connect(minter).mint(thirdParty.address, initialBalance)
+    })
+
+    after(async () => {
+      await restoreSnapshot()
+    })
+
+    context("when burning more than balance", () => {
+      it("should revert", async () => {
+        await expect(
+          token.connect(tokenHolder).burn(initialBalance.add(1))
+        ).to.be.revertedWith("ERC20: burn amount exceeds balance")
+      })
+    })
+
+    const describeBurn = (description: string, amount: BigNumber) => {
+      describe(description, () => {
+        let burnTx: ContractTransaction
+
+        before(async () => {
+          await createSnapshot()
+          burnTx = await token.connect(tokenHolder).burn(amount)
+        })
+
+        after(async () => {
+          await restoreSnapshot()
+        })
+
+        it("should decrement totalSupply", async () => {
+          const expectedSupply = initialSupply.sub(amount)
+          expect(await token.totalSupply()).to.equal(expectedSupply)
+        })
+
+        it("should decrement account's balance", async () => {
+          const expectedBalance = initialBalance.sub(amount)
+          expect(await token.balanceOf(tokenHolder.address)).to.equal(
+            expectedBalance
+          )
+        })
+
+        it("should emit Transfer event", async () => {
+          await expect(burnTx)
+            .to.emit(token, "Transfer")
+            .withArgs(tokenHolder.address, ZERO_ADDRESS, amount)
+        })
+      })
+    }
+
+    describeBurn("for the entire balance", initialBalance)
+    describeBurn("for less than the entire balance", initialBalance.sub(1))
+  })
+
+  describe("burnFrom", () => {
+    const initialSupply = to1e18(36)
+    const initialBalance = to1e18(18)
+
+    before(async () => {
+      await createSnapshot()
+      await token.connect(governance).addMinter(minter.address)
+      await token.connect(minter).mint(tokenHolder.address, initialBalance)
+      await token.connect(minter).mint(thirdParty.address, initialBalance)
+    })
+
+    after(async () => {
+      await restoreSnapshot()
+    })
+
+    context("when burning more than the balance", () => {
+      it("should revert", async () => {
+        await token
+          .connect(tokenHolder)
+          .approve(thirdParty.address, initialBalance.add(1))
+        await expect(
+          token
+            .connect(thirdParty)
+            .burnFrom(tokenHolder.address, initialBalance.add(1))
+        ).to.be.revertedWith("ERC20: burn amount exceeds balance")
+      })
+    })
+
+    context("when burning more than the allowance", () => {
+      it("should revert", async () => {
+        await token
+          .connect(tokenHolder)
+          .approve(thirdParty.address, initialBalance.sub(1))
+        await expect(
+          token
+            .connect(thirdParty)
+            .burnFrom(tokenHolder.address, initialBalance)
+        ).to.be.revertedWith("ERC20: insufficient allowance")
+      })
+    })
+
+    const describeBurnFrom = (description: string, amount: BigNumber) => {
+      describe(description, () => {
+        let burnTx: ContractTransaction
+
+        before(async () => {
+          await createSnapshot()
+          await token.connect(tokenHolder).approve(thirdParty.address, amount)
+          burnTx = await token
+            .connect(thirdParty)
+            .burnFrom(tokenHolder.address, amount)
+        })
+
+        after(async () => {
+          await restoreSnapshot()
+        })
+
+        it("should decrement totalSupply", async () => {
+          const expectedSupply = initialSupply.sub(amount)
+          expect(await token.totalSupply()).to.equal(expectedSupply)
+        })
+
+        it("should decrement account's balance", async () => {
+          const expectedBalance = initialBalance.sub(amount)
+          expect(await token.balanceOf(tokenHolder.address)).to.equal(
+            expectedBalance
+          )
+        })
+
+        it("should decrement allowance", async () => {
+          const allowance = await token.allowance(
+            tokenHolder.address,
+            thirdParty.address
+          )
+
+          expect(allowance).to.equal(0)
+        })
+
+        it("should emit Transfer event", async () => {
+          await expect(burnTx)
+            .to.emit(token, "Transfer")
+            .withArgs(tokenHolder.address, ZERO_ADDRESS, amount)
+        })
+      })
+    }
+
+    describeBurnFrom("for the entire balance", initialBalance)
+    describeBurnFrom("for less than the balance", initialBalance.sub(1))
   })
 })
