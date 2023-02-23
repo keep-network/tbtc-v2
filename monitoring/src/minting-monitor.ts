76bfa9a3305b370203dd6d6f73545a99e212491e
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2023-02-23 19:45:07 +0100

    Monitor minting not requested by any minter

--- monitoring/src/minting-monitor.ts
+++ monitoring/src/minting-monitor.ts
@@ -1,9 +1,12 @@
+import { OptimisticMinting } from "@keep-network/tbtc-v2.ts"
+
 import { SystemEventType } from "./system-event"
 
 import type {
   OptimisticMintingCancelledEvent as OptimisticMintingCancelledChainEvent,
   OptimisticMintingRequestedEvent as OptimisticMintingRequestedChainEvent,
 } from "@keep-network/tbtc-v2.ts/dist/src/optimistic-minting"
+import type { DepositRevealedEvent as DepositRevealedChainEvent } from "@keep-network/tbtc-v2.ts/dist/src/deposit"
 import type {
   Bridge,
   Identifier,
@@ -15,6 +18,26 @@ import type { SystemEvent, Monitor as SystemEventMonitor } from "./system-event"
 import type { BigNumber } from "ethers"
 
 const satoshiMultiplier = 1e10
+
+// Number of blocks the monitor gives for minters to create a minting
+// request for the given deposit, counted from the deposit reveal block.
+// The value is 2 hours which is 600 blocks assuming 12 seconds for ETH block.
+// This value aims to include:
+// - worst case 6 BTC confirmations of the deposit, i.e. ~60 min assuming
+//   10 min for BTC block
+// - 20 minutes of the designated minter precedence period
+// - 40 minutes of additional margin
+const mintingRequestTimeoutBlocks = 600
+
+// Number of blocks the monitor gives for minters to finalize the
+// minting request, counted from the minting request block.
+// The value is the current optimistic minting delay plus 1 hour, converted
+// to blocks assuming 12 seconds for ETH block. The value aims to include:
+// - optimistic minting delay
+// - 20 minutes of the designated minter precedence period
+// - 40 minutes of additional margin
+const mintingFinalizationTimeoutBlocks = (optimisticMintingDelay: number) =>
+  optimisticMintingDelay / 12 + 300
 
 const OptimisticMintingCancelled = (
   chainEvent: OptimisticMintingCancelledChainEvent
@@ -79,11 +102,11 @@ const OptimisticMintingRequestedForUndeterminedBtcTx = (
   block: chainEvent.blockNumber,
 })
 
-const DesignatedMinterNotRequestedMinting = (
+const OptimisticMintingNotRequestedByDesignatedMinter = (
   chainEvent: OptimisticMintingRequestedChainEvent,
   designatedMinter: Identifier
 ): SystemEvent => ({
-  title: "Designated minter has not requested minting",
+  title: "Optimistic minting was not requested by designated minter",
   type: SystemEventType.Warning,
   data: {
     actualMinter: `0x${chainEvent.minter.identifierHex}`,
@@ -98,12 +121,27 @@ const DesignatedMinterNotRequestedMinting = (
   block: chainEvent.blockNumber,
 })
 
+const OptimisticMintingNotRequestedByAnyMinter = (
+  chainEvent: DepositRevealedChainEvent
+): SystemEvent => ({
+  title: "Optimistic minting was not requested by any minter",
+  type: SystemEventType.Warning,
+  data: {
+    btcFundingTxHash: chainEvent.fundingTxHash.toString(),
+    btcFundingOutputIndex: chainEvent.fundingOutputIndex.toString(),
+    amountSat: chainEvent.amount.toString(),
+    ethRevealTxHash: chainEvent.transactionHash.toPrefixedString(),
+  },
+  block: chainEvent.blockNumber,
+})
+
 // Cache that holds some chain data relevant for the minting monitor.
 // Allows fetching the data once and reusing them multiple times across the monitor.
 type ChainDataCache = {
   mintingCancelledEvents: OptimisticMintingCancelledChainEvent[]
   mintingRequestedEvents: OptimisticMintingRequestedChainEvent[]
   minters: Identifier[]
+  optimisticMintingDelay: number
 }
 
 export class MintingMonitor implements SystemEventMonitor {
@@ -129,6 +167,9 @@ export class MintingMonitor implements SystemEventMonitor {
     systemEvents.push(...this.checkMintingCancels(cache))
     systemEvents.push(...(await this.checkMintingRequestsValidity(cache)))
     systemEvents.push(...this.checkDesignatedMintersHealth(cache))
+    systemEvents.push(
+      ...(await this.checkOrphanedMinting(cache, fromBlock, toBlock))
+    )
 
     // eslint-disable-next-line no-console
     console.log("completed minting monitor check")
@@ -151,6 +192,7 @@ export class MintingMonitor implements SystemEventMonitor {
       mintingRequestedEvents:
         await this.tbtcVault.getOptimisticMintingRequestedEvents(options),
       minters: await this.tbtcVault.getMinters(),
+      optimisticMintingDelay: await this.tbtcVault.optimisticMintingDelay(),
     }
   }
 
@@ -232,7 +274,10 @@ export class MintingMonitor implements SystemEventMonitor {
       }))
       .filter((mre) => !mre.minter.equals(mre.designatedMinter))
       .map((mre) =>
-        DesignatedMinterNotRequestedMinting(mre, mre.designatedMinter)
+        OptimisticMintingNotRequestedByDesignatedMinter(
+          mre,
+          mre.designatedMinter
+        )
       )
       .forEach((se) => systemEvents.push(se))
 
@@ -253,5 +298,53 @@ export class MintingMonitor implements SystemEventMonitor {
     const index = (d ^ f) % minters.length
 
     return minters[index]
+  }
+
+  private async checkOrphanedMinting(
+    cache: ChainDataCache,
+    fromBlock: number,
+    toBlock: number
+  ) {
+    const systemEvents: SystemEvent[] = []
+
+    systemEvents.push(
+      ...(await this.checkMintingNotRequested(fromBlock, toBlock))
+    )
+
+    // TODO: Check minting not finalized.
+
+    return systemEvents
+  }
+
+  private async checkMintingNotRequested(fromBlock: number, toBlock: number) {
+    const rewindBlock = (block: number, shift: number) =>
+      block - shift > 0 ? block - shift : 0
+
+    // We need to rewind the block window by the minting request timeout.
+    // This way, we are looking for past deposits whose time for creating
+    // the minting request was already elapsed.
+    const chainEvents = await this.bridge.getDepositRevealedEvents({
+      fromBlock: rewindBlock(fromBlock, mintingRequestTimeoutBlocks),
+      toBlock: rewindBlock(toBlock, mintingRequestTimeoutBlocks),
+    })
+
+    const mintingRequests = await Promise.allSettled(
+      chainEvents.map((ce) =>
+        OptimisticMinting.getOptimisticMintingRequest(
+          ce.fundingTxHash,
+          ce.fundingOutputIndex,
+          this.tbtcVault
+        )
+      )
+    )
+
+    return chainEvents
+      .filter((ce, index) => {
+        const request = mintingRequests[index]
+        const requestExists =
+          request.status === "fulfilled" && request.value.requestedAt !== 0
+        return !requestExists
+      })
+      .map(OptimisticMintingNotRequestedByAnyMinter)
   }
 }
