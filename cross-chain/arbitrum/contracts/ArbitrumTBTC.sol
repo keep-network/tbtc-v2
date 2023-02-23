16ecc251b6d7dac0de9b7d3b3023b32fd4e76486
Dmitry <dymitr.paremski@keep.network>
2023-02-23 13:51:17 +0100

    Drafting Readme

--- cross-chain/arbitrum/contracts/ArbitrumTBTC.sol
+++ cross-chain/arbitrum/contracts/ArbitrumTBTC.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.17;
 
 import "./L2TBTC.sol";
 
+/// @notice Canonical tBTC Token on Arbitrum
 contract ArbitrumTBTC is L2TBTC {
     function initialize() external {
         initialize("ArbitrumTBTC", "arbitrumTBTC"); // TODO: what should we name it?
