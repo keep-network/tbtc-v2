fa8f8fdf6cd2bfb6d15e957d211700286ae10af2
Dmitry <dymitr.paremski@keep.network>
2023-02-23 13:49:52 +0100

    Initializing ArbitrumTBTC token

--- cross-chain/arbitrum/deploy/01_deploy_arbitrum_tbtc_token.ts
+++ cross-chain/arbitrum/deploy/01_deploy_arbitrum_tbtc_token.ts
@@ -3,13 +3,19 @@ import { DeployFunction } from "hardhat-deploy/types"
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments, getNamedAccounts, helpers } = hre
-  const { deploy } = deployments
+  const { deploy, execute } = deployments
   const { deployer } = await getNamedAccounts()
 
   const arbitrumTBTC = await deploy("ArbitrumTBTC", {
     from: deployer,
     log: true,
   })
+
+  await execute(
+    "ArbitrumTBTC",
+    { from: deployer, log: true, waitConfirmations: 1 },
+    "initialize",
+  )
 
   if (hre.network.tags.etherscan) {
     await helpers.etherscan.verify(arbitrumTBTC)
