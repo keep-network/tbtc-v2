07e892487e0515fc8ae940d33823a6d41feec97d
Dmitry <dymitr.paremski@keep.network>
2023-03-01 13:45:07 +0100

    Adding Wormhole interfaces

---
+++ cross-chain/arbitrum/contracts/interfaces/IWETH.sol
@@ -0,0 +1,13 @@
+// contracts/Bridge.sol
+// SPDX-License-Identifier: Apache 2
+
+// Copy from https://github.com/wormhole-foundation/wormhole/blob/main/ethereum/contracts/bridge/interfaces/IWETH.sol
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+interface IWETH is IERC20 {
+    function deposit() external payable;
+    function withdraw(uint amount) external;
+}
