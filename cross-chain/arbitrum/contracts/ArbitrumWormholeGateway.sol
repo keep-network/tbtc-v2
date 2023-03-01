349cfe6bf641996ed33ebc641dc323e4778c8f88
Dmitry <dymitr.paremski@keep.network>
2023-03-01 13:49:39 +0100

    Drafting ArbtitrumWormholeGateway

--- cross-chain/arbitrum/contracts/ArbitrumWormholeGateway.sol
+++ cross-chain/arbitrum/contracts/ArbitrumWormholeGateway.sol
@@ -1,10 +1,16 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
-pragma solidity 0.8.17;
+pragma solidity ^0.8.17;
+
+import "./interfaces/ITokenBridge.sol";
 
 /// @notice This contract acts as a vending machine converting
 ///         'wormholeArbitrumTBTC <-> arbitrumTBTC'
 // TODO: inherit from a generic L2WormholeGateway
 contract ArbitrumWormholeGateway {
-    // TODO: implement
+    ITokenBridge public tokenBridge;
+
+    function initialize(address wormholeTokenBridgeAddress) external {
+        tokenBridge = ITokenBridge(wormholeTokenBridgeAddress);
+    }
 }
