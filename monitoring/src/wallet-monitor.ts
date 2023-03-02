9d687746f2f6ff2782e2e3b6ff7f6d8270758a71
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2023-03-02 15:40:08 +0100

    Add DKG lifecycle to the wallet monitor
    Here we add the monitoring of the DKG lifecycle events. Specific events
    are `DkgResultSubmitted`, `DkgResultApproved` and `DkgResultChallenged`.

--- monitoring/src/wallet-monitor.ts
+++ monitoring/src/wallet-monitor.ts
@@ -1,7 +1,15 @@
 import { SystemEventType } from "./system-event"
 
-import type { NewWalletRegisteredEvent as WalletRegisteredChainEvent } from "@keep-network/tbtc-v2.ts/dist/src/wallet"
-import type { Bridge } from "@keep-network/tbtc-v2.ts/dist/src/chain"
+import type {
+  NewWalletRegisteredEvent as WalletRegisteredChainEvent,
+  DkgResultSubmittedEvent as DkgResultSubmittedChainEvent,
+  DkgResultApprovedEvent as DkgResultApprovedChainEvent,
+  DkgResultChallengedEvent as DkgResultChallengedChainEvent,
+} from "@keep-network/tbtc-v2.ts/dist/src/wallet"
+import type {
+  Bridge,
+  WalletRegistry,
+} from "@keep-network/tbtc-v2.ts/dist/src/chain"
 import type { SystemEvent, Monitor as SystemEventMonitor } from "./system-event"
 
 const WalletRegistered = (
@@ -17,6 +25,56 @@ const WalletRegistered = (
   block: chainEvent.blockNumber,
 })
 
+const DkgResultSubmitted = (
+  chainEvent: DkgResultSubmittedChainEvent
+): SystemEvent => ({
+  title: "DKG result submitted",
+  type: SystemEventType.Informational,
+  data: {
+    resultHash: chainEvent.resultHash.toPrefixedString(),
+    seed: chainEvent.seed.toPrefixedString(),
+    submitterMemberIndex: chainEvent.result.submitterMemberIndex.toString(),
+    groupPubKey: chainEvent.result.groupPubKey.toPrefixedString(),
+    misbehavedMembersIndices:
+      chainEvent.result.misbehavedMembersIndices.toString(),
+    signatures: chainEvent.result.signatures.toPrefixedString(),
+    signingMembersIndices: chainEvent.result.signingMembersIndices
+      .map((smi) => smi.toString())
+      .toString(),
+    members: chainEvent.result.members.toString(),
+    membersHash: chainEvent.result.membersHash.toPrefixedString(),
+    ethSubmitTxHash: chainEvent.transactionHash.toPrefixedString(),
+  },
+  block: chainEvent.blockNumber,
+})
+
+const DkgResultApproved = (
+  chainEvent: DkgResultApprovedChainEvent
+): SystemEvent => ({
+  title: "DKG result approved",
+  type: SystemEventType.Informational,
+  data: {
+    resultHash: chainEvent.resultHash.toPrefixedString(),
+    approver: `0x${chainEvent.approver.identifierHex}`,
+    ethApproveTxHash: chainEvent.transactionHash.toPrefixedString(),
+  },
+  block: chainEvent.blockNumber,
+})
+
+const DkgResultChallenged = (
+  chainEvent: DkgResultChallengedChainEvent
+): SystemEvent => ({
+  title: "DKG result challenged",
+  type: SystemEventType.Critical,
+  data: {
+    resultHash: chainEvent.resultHash.toPrefixedString(),
+    challenger: `0x${chainEvent.challenger.identifierHex}`,
+    reason: chainEvent.reason,
+    ethChallengeTxHash: chainEvent.transactionHash.toPrefixedString(),
+  },
+  block: chainEvent.blockNumber,
+})
+
 export class WalletMonitor implements SystemEventMonitor {
   private bridge: Bridge
 
@@ -28,14 +86,74 @@ export class WalletMonitor implements SystemEventMonitor {
     // eslint-disable-next-line no-console
     console.log("running wallet monitor check")
 
-    const chainEvents = await this.bridge.getNewWalletRegisteredEvents({
-      fromBlock,
-      toBlock,
-    })
+    const walletRegistry = await this.bridge.walletRegistry()
+
+    const systemEvents: SystemEvent[] = []
+    systemEvents.push(...(await this.checkNewWallets(fromBlock, toBlock)))
+    systemEvents.push(
+      ...(await this.checkDkgSubmissions(walletRegistry, fromBlock, toBlock))
+    )
+    systemEvents.push(
+      ...(await this.checkDkgApprovals(walletRegistry, fromBlock, toBlock))
+    )
+    systemEvents.push(
+      ...(await this.checkDkgChallenges(walletRegistry, fromBlock, toBlock))
+    )
 
     // eslint-disable-next-line no-console
     console.log("completed wallet monitor check")
 
-    return chainEvents.map(WalletRegistered)
+    return systemEvents
+  }
+
+  private async checkNewWallets(
+    fromBlock: number,
+    toBlock: number
+  ): Promise<SystemEvent[]> {
+    return (
+      await this.bridge.getNewWalletRegisteredEvents({
+        fromBlock,
+        toBlock,
+      })
+    ).map(WalletRegistered)
+  }
+
+  private async checkDkgSubmissions(
+    walletRegistry: WalletRegistry,
+    fromBlock: number,
+    toBlock: number
+  ): Promise<SystemEvent[]> {
+    return (
+      await walletRegistry.getDkgResultSubmittedEvents({
+        fromBlock,
+        toBlock,
+      })
+    ).map(DkgResultSubmitted)
+  }
+
+  private async checkDkgApprovals(
+    walletRegistry: WalletRegistry,
+    fromBlock: number,
+    toBlock: number
+  ): Promise<SystemEvent[]> {
+    return (
+      await walletRegistry.getDkgResultApprovedEvents({
+        fromBlock,
+        toBlock,
+      })
+    ).map(DkgResultApproved)
+  }
+
+  private async checkDkgChallenges(
+    walletRegistry: WalletRegistry,
+    fromBlock: number,
+    toBlock: number
+  ): Promise<SystemEvent[]> {
+    return (
+      await walletRegistry.getDkgResultChallengedEvents({
+        fromBlock,
+        toBlock,
+      })
+    ).map(DkgResultChallenged)
   }
 }
