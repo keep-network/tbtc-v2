8890135871a7125ad177215a6584e8a8d9cfee02
Dmitry <dymitr.paremski@keep.network>
2023-03-08 15:27:49 +0100

    Removing tenderly verification and adding L2 prefix

--- cross-chain/arbitrum/deploy_l2/02_deploy_arbitrum_wormhole_gateway.ts
+++ cross-chain/arbitrum/deploy_l2/02_deploy_arbitrum_wormhole_gateway.ts
@@ -9,45 +9,39 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { log } = deployments
   const { deployer } = await getNamedAccounts()
 
-  // const TokenBridge = await hre.companionNetworks['l1'].deployments.get(
-  //   'TokenBridge'
-  // ); // TokenBridge on L1
-
-  const TokenBridge = await deployments.getOrNull("TokenBridge") // L2
+  const L2TokenBridge = await deployments.getOrNull("TokenBridge") // L2
   let tokenBridgeAddress = ""
 
-  if (TokenBridge && helpers.address.isValid(TokenBridge.address)) {
-    log(`using existing L2 TokenBridge at ${TokenBridge.address}`)
-    tokenBridgeAddress = TokenBridge.address
+  if (L2TokenBridge && helpers.address.isValid(L2TokenBridge.address)) {
+    log(`using existing L2 TokenBridge at ${L2TokenBridge.address}`)
+    tokenBridgeAddress = L2TokenBridge.address
   } else {
     // For local development
     const FakeTokenBridge = await smock.fake<ITokenBridge>("ITokenBridge")
     tokenBridgeAddress = FakeTokenBridge.address
   }
 
-  const [arbitrumWormholeGateway, proxyDeployment] =
-    await helpers.upgrades.deployProxy("ArbitrumWormholeGateway", {
+  const [, proxyDeployment] = await helpers.upgrades.deployProxy(
+    "ArbitrumWormholeGateway",
+    {
       initializerArgs: [tokenBridgeAddress],
       factoryOpts: { signer: await ethers.getSigner(deployer) },
       proxyOpts: {
         kind: "transparent",
       },
-    })
+    }
+  )
 
-  if (hre.network.tags.etherscan) {
+  if (hre.network.tags.arbiscan) {
     // We use `verify` instead of `verify:verify` as the `verify` task is defined
-    // in "@openzeppelin/hardhat-upgrades" to perform Etherscan verification
-    // of Proxy and Implementation contracts.
+    // in "@openzeppelin/hardhat-upgrades" to verify the proxy’s implementation
+    // contract, the proxy itself and any proxy-related contracts, as well as
+    // link the proxy to the implementation contract’s ABI on (Ether)scan.
     await hre.run("verify", {
       address: proxyDeployment.address,
       constructorArgsParams: proxyDeployment.args,
-    })
-  }
-
-  if (hre.network.tags.tenderly) {
-    await hre.tenderly.verify({
-      name: "ArbitrumWormholeGateway",
-      address: arbitrumWormholeGateway.address,
+      // Implementation contract
+      contract: "contracts/ArbitrumWormholeGateway.sol:ArbitrumWormholeGateway",
     })
   }
 }
