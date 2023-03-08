8cf7248dd302d6a78943127556fa718f4a7bca07
Dmitry <dymitr.paremski@keep.network>
2023-03-08 22:42:31 +0100

    Fixing name of a transferred contract
    Changed from ArbitrumTBTC to ArbitrumWormholeGateway

--- cross-chain/arbitrum/deploy_l2/12_transfer_arbitrum_wormhole_gateway_ownership.ts
+++ cross-chain/arbitrum/deploy_l2/12_transfer_arbitrum_wormhole_gateway_ownership.ts
@@ -5,10 +5,10 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { getNamedAccounts, helpers } = hre
   const { deployer, governance } = await getNamedAccounts()
 
-  await helpers.ownable.transferOwnership("ArbitrumTBTC", governance, deployer)
+  await helpers.ownable.transferOwnership("ArbitrumWormholeGateway", governance, deployer)
 }
 
 export default func
 
-func.tags = ["TransferArbitrumTBTCOwnership"]
-func.dependencies = ["ArbitrumTBTC"]
+func.tags = ["TransferArbitrumWormholeGatewayOwnership"]
+func.dependencies = ["ArbitrumWormholeGateway"]
