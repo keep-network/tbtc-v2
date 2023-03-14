d8aea1f8d5dc537d9d3fdb1d320465a094fadeca
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2023-03-14 14:00:10 +0100

    Wallet coordinator contract

---
+++ solidity/contracts/bridge/WalletCoordinator.sol
@@ -0,0 +1,125 @@
+// SPDX-License-Identifier: GPL-3.0-only
+
+// ██████████████     ▐████▌     ██████████████
+// ██████████████     ▐████▌     ██████████████
+//               ▐████▌    ▐████▌
+//               ▐████▌    ▐████▌
+// ██████████████     ▐████▌     ██████████████
+// ██████████████     ▐████▌     ██████████████
+//               ▐████▌    ▐████▌
+//               ▐████▌    ▐████▌
+//               ▐████▌    ▐████▌
+//               ▐████▌    ▐████▌
+//               ▐████▌    ▐████▌
+//               ▐████▌    ▐████▌
+
+pragma solidity 0.8.17;
+
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
+import "./Bridge.sol";
+import "./Wallets.sol";
+
+// TODO: Documentation and unit tests.
+// TODO: Should we make it pausable?
+contract WalletCoordinator is OwnableUpgradeable {
+    struct DepositSweepProposal {
+        bytes20 walletPubKeyHash;
+        bytes32[] fundingTxHash;
+        uint32[] fundingOutputIndex;
+    }
+
+    Bridge internal bridge;
+
+    mapping(bytes20 => uint32) public walletLock;
+
+    // TODO: Make it governable.
+    mapping(address => bool) public isProposalSubmitter;
+
+    // TODO: Make it governable.
+    uint32 public depositSweepProposalValidity;
+
+    event DepositSweepProposalSubmitted(
+        DepositSweepProposal proposal,
+        address submitter
+    );
+
+    modifier onlyProposalSubmitter() {
+        require(
+            isProposalSubmitter[msg.sender],
+            "Caller is not proposal submitter"
+        );
+        _;
+    }
+
+    modifier onlyAfterWalletLock(bytes20 walletPubKeyHash) {
+        require(
+            /* solhint-disable-next-line not-rely-on-time */
+            block.timestamp > walletLock[walletPubKeyHash],
+            "Wallet locked"
+        );
+        _;
+    }
+
+    function initialize(Bridge _bridge) external initializer {
+        __Ownable_init();
+
+        bridge = _bridge;
+        depositSweepProposalValidity = 4 hours;
+    }
+
+    function unlockWallet(bytes20 walletPubKeyHash) external onlyOwner {
+        // Just in case, allow the owner to unlock the wallet earlier.
+        walletLock[walletPubKeyHash] = 0;
+    }
+
+    function submitDepositSweepProposal(DepositSweepProposal calldata proposal)
+        external
+        onlyProposalSubmitter
+        onlyAfterWalletLock(proposal.walletPubKeyHash)
+    {
+        walletLock[proposal.walletPubKeyHash] =
+            /* solhint-disable-next-line not-rely-on-time */
+            uint32(block.timestamp) +
+            depositSweepProposalValidity;
+
+        emit DepositSweepProposalSubmitted(proposal, msg.sender);
+    }
+
+    function validateDepositSweepProposal(
+        DepositSweepProposal calldata proposal
+    ) external view {
+        require(
+            bridge.wallets(proposal.walletPubKeyHash).state ==
+                Wallets.WalletState.Live,
+            "Wallet is not in Live state"
+        );
+
+        require(
+            proposal.fundingTxHash.length == proposal.fundingOutputIndex.length,
+            "Arrays must have the same length"
+        );
+
+        // TODO: Check against the governable maximum swept deposits limit.
+
+        for (uint256 i = 0; i < proposal.fundingTxHash.length; i++) {
+            uint256 depositKey = uint256(
+                keccak256(
+                    abi.encodePacked(
+                        proposal.fundingTxHash[i],
+                        proposal.fundingOutputIndex[i]
+                    )
+                )
+            );
+
+            Deposit memory deposit = bridge.deposits(depositKey);
+
+            require(deposit.sweptAt == 0, "Deposit already swept");
+
+            // TODO: Check deposit was revealed enough time ago to ensure block finality.
+            // TODO: Check deposit will not become refundable soon.
+        }
+
+        // TODO: What else?
+    }
+}
