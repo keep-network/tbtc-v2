bb63f2b5fb7aefbb87aa7f0dc2f836973595166b
Dmitry <dymitr.paremski@keep.network>
2023-04-14 15:01:07 +0200

    Unique tags for Polygon wormhole mappings

--- cross-chain/polygon/deploy_sidechain/14_update_with_optimism_in_wormhole_gateway_mapping.ts
+++ cross-chain/polygon/deploy_sidechain/14_update_with_optimism_in_wormhole_gateway_mapping.ts
@@ -37,5 +37,5 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 
 export default func
 
-func.tags = ["SetGatewayAddress"]
+func.tags = ["SetOptimismGatewayAddress"]
 func.dependencies = ["PolygonWormholeGateway"]
