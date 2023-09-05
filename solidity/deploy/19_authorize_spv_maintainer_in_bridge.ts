28eaf9454b4a5fddd6c8c860022d06b2d1ef8af2
Michalina <michalina.cienciala@akena.co>
2023-09-05 11:40:13 +0200

    Support deployment on Sepolia
    The Görli testnet currently used by Threshold/Keep for development purposes is
    planned to become deprecated with the end of year 2023. The testnet that is
    planned to replace it is called
    [Holešky](https://github.com/eth-clients/holesky), however it's not yet
    available - it's planned it will become widely accessible on Oct 1, 2023
    ([source](https://everstake.one/blog/new-ethereum-testnet-holesky-all-you-need-to-know-now)).
    Switching our infrastructure to support new testnet is quite time consuming, so
    moving directly from Görli to Holešky may be quite risky, especially if there
    would be some delays in the date of Holešky genesis (not meeting the planned
    timelines is not a rare occurrence in the Ethereum space). As a solution, we
    decided to switch first to another testnet that is currently live - Sepolia.
    This testnet's EOL is planned for 2026, which gives us plenty of time to move to
    Holešky before Sepolia gets deprecated.

--- solidity/deploy/19_authorize_spv_maintainer_in_bridge.ts
+++ solidity/deploy/19_authorize_spv_maintainer_in_bridge.ts
@@ -21,9 +21,9 @@ func.tags = ["AuthorizeSpvMaintainer"]
 func.dependencies = ["Bridge"]
 
 // SPV maintainer can submit SPV proofs to the Bridge. We authorize spvMaintainer
-// account for Hardhat network (unit tests) and Goerli (testnet) but we DO NOT
-// want to authorize it for Mainnet deployment. SPV maintainer will be authorized
-// separately by the Governance when sweeping will be activated.
+// account for Hardhat network (unit tests) and Goerli/Sepolia (testnets) but we
+// DO NOT want to authorize it for Mainnet deployment. SPV maintainer will be
+// authorized separately by the Governance when sweeping will be activated.
 //
 // Note that at this point MaintainerProxy contract is already authorized in the
 // Bridge (see AuthorizeMaintainerProxyInBridge tag).
--- solidity/hardhat.config.ts
+++ solidity/hardhat.config.ts
@@ -117,6 +117,14 @@ const config: HardhatUserConfig = {
         : undefined,
       tags: ["tenderly"],
     },
+    sepolia: {
+      url: process.env.CHAIN_API_URL || "",
+      chainId: 11155111,
+      accounts: process.env.ACCOUNTS_PRIVATE_KEYS
+        ? process.env.ACCOUNTS_PRIVATE_KEYS.split(",")
+        : undefined,
+      tags: ["tenderly"],
+    },
     mainnet: {
       url: process.env.CHAIN_API_URL || "",
       chainId: 1,
@@ -172,6 +180,11 @@ const config: HardhatUserConfig = {
         "node_modules/@keep-network/random-beacon/artifacts",
         "node_modules/@keep-network/ecdsa/artifacts",
       ],
+      sepolia: [
+        "node_modules/@keep-network/tbtc/artifacts",
+        "node_modules/@keep-network/random-beacon/artifacts",
+        "node_modules/@keep-network/ecdsa/artifacts",
+      ],
       mainnet: ["./external/mainnet"],
     },
   },
@@ -180,16 +193,19 @@ const config: HardhatUserConfig = {
     deployer: {
       default: 1,
       goerli: 0,
+      sepolia: 0,
       mainnet: 0, // "0x123694886DBf5Ac94DDA07135349534536D14cAf"
     },
     governance: {
       default: 2,
       goerli: 0,
+      sepolia: 0,
       mainnet: "0x9f6e831c8f8939dc0c830c6e492e7cef4f9c2f5f", // Threshold Council
     },
     chaosnetOwner: {
       default: 3,
       goerli: 0,
+      sepolia: 0,
       // Not used for mainnet deployment scripts of `@keepn-network/tbtc-v2`.
       // Used by `@keep-network/random-beacon` and `@keep-network/ecdsa`
       // when deploying `SortitionPool`s.
@@ -197,36 +213,43 @@ const config: HardhatUserConfig = {
     esdm: {
       default: 4,
       goerli: 0,
+      sepolia: 0,
       mainnet: "0x9f6e831c8f8939dc0c830c6e492e7cef4f9c2f5f", // Threshold Council
     },
     keepTechnicalWalletTeam: {
       default: 5,
       goerli: 0,
+      sepolia: 0,
       mainnet: "0xB3726E69Da808A689F2607939a2D9E958724FC2A",
     },
     keepCommunityMultiSig: {
       default: 6,
       goerli: 0,
+      sepolia: 0,
       mainnet: "0x19FcB32347ff4656E4E6746b4584192D185d640d",
     },
     treasury: {
       default: 7,
       goerli: 0,
+      sepolia: 0,
       mainnet: "0x87F005317692D05BAA4193AB0c961c69e175f45f", // Token Holder DAO
     },
     spvMaintainer: {
       default: 8,
       goerli: 0,
+      sepolia: 0,
       // We are not setting SPV maintainer for mainnet in deployment scripts.
     },
     coordinator: {
       default: 9,
       goerli: "0x4815cd81fFc21039a25aCFbD97CE75cCE8579042",
+      // TODO: add one for sepolia
       mainnet: "0x0595acCca29654c43Bd67E18578b30a405265234",
     },
     v1Redeemer: {
       default: 10,
       goerli: 0,
+      sepolia: 0,
       mainnet: "0x8Bac178fA95Cb56D11A94d4f1b2B1F5Fc48A30eA",
     },
   },
