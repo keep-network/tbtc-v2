1cbdcfc7284b914fa88df7a1cd5f0873adfad922
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2023-10-09 17:38:37 +0200

    Upgrade `OptimismWormholeGateway` script

---
+++ cross-chain/optimism/deploy_l2/32_manual_upgrade_optimism_wormhole_gateway.ts
@@ -0,0 +1,77 @@
+import type { Artifact, HardhatRuntimeEnvironment } from "hardhat/types"
+import type { DeployFunction, Deployment } from "hardhat-deploy/types"
+import { ContractFactory } from "ethers"
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { ethers, helpers, deployments } = hre
+
+  const { deployer } = await helpers.signers.getNamedSigners()
+
+  const proxyDeployment: Deployment = await deployments.get(
+    "OptimismWormholeGateway"
+  )
+  const implementationContractFactory: ContractFactory =
+    await ethers.getContractFactory("L2WormholeGateway", {
+      signer: deployer,
+    })
+
+  // Deploy new implementation contract
+  const newImplementationAddress: string = (await hre.upgrades.prepareUpgrade(
+    proxyDeployment,
+    implementationContractFactory,
+    {
+      kind: "transparent",
+    }
+  )) as string
+
+  deployments.log(
+    `new implementation contract deployed at: ${newImplementationAddress}`
+  )
+
+  // Assemble proxy upgrade transaction.
+  const proxyAdmin = await hre.upgrades.admin.getInstance()
+  const proxyAdminOwner = await proxyAdmin.owner()
+
+  const upgradeTxData = await proxyAdmin.interface.encodeFunctionData(
+    "upgrade",
+    [proxyDeployment.address, newImplementationAddress]
+  )
+
+  deployments.log(
+    `proxy admin owner ${proxyAdminOwner} is required to upgrade proxy implementation with transaction:\n` +
+      `\t\tfrom: ${proxyAdminOwner}\n` +
+      `\t\tto: ${proxyAdmin.address}\n` +
+      `\t\tdata: ${upgradeTxData}`
+  )
+
+  // Update Deployment Artifact
+  const gatewayArtifact: Artifact =
+    hre.artifacts.readArtifactSync("L2WormholeGateway")
+
+  await deployments.save("OptimismWormholeGateway", {
+    ...proxyDeployment,
+    abi: gatewayArtifact.abi,
+    implementation: newImplementationAddress,
+  })
+
+  // Contracts can be verified on L2 Optimism Etherscan in a similar way as we do it on
+  // L1 Etherscan
+  if (hre.network.tags.optimism_etherscan) {
+    // We use `verify` instead of `verify:verify` as the `verify` task is defined
+    // in "@openzeppelin/hardhat-upgrades" to verify the proxy’s implementation
+    // contract, the proxy itself and any proxy-related contracts, as well as
+    // link the proxy to the implementation contract’s ABI on (Ether)scan.
+    await hre.run("verify", {
+      address: newImplementationAddress,
+      constructorArgsParams: proxyDeployment.args,
+    })
+  }
+}
+
+export default func
+
+func.tags = ["ManualUpgradeOptimismWormholeGateway"]
+
+// Comment this line when running an upgrade.
+// yarn deploy --tags ManualUpgradeOptimismWormholeGateway --network <network>
+func.skip = async () => true
