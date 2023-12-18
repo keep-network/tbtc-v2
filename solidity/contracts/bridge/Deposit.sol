481d0201b28931700ad394a578f09d9b71552966
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2023-12-18 19:47:32 +0100

    Add documentation to the recent changes around `Bridge` and extra data

--- solidity/contracts/bridge/Deposit.sol
+++ solidity/contracts/bridge/Deposit.sol
@@ -44,6 +44,25 @@ import "./Wallets.sol";
 ///      Since each depositor has their own Ethereum address and their own
 ///      blinding factor, each depositor’s script is unique, and the hash
 ///      of each depositor’s script is unique.
+///
+///      This library also supports another variant of the deposit script
+///      allowing to embed 32-byte extra data. The extra data allows to attach
+///      additional context to the deposit. The script with 32-byte extra data
+///      looks like this:
+///
+///      ```
+///      <depositorAddress> DROP
+///      <extraData> DROP
+///      <blindingFactor> DROP
+///      DUP HASH160 <walletPubKeyHash> EQUAL
+///      IF
+///        CHECKSIG
+///      ELSE
+///        DUP HASH160 <refundPubkeyHash> EQUALVERIFY
+///        <refundLocktime> CHECKLOCKTIMEVERIFY DROP
+///        CHECKSIG
+///      ENDIF
+///      ```
 library Deposit {
     using BTCUtils for bytes;
     using BytesLib for bytes;
@@ -157,7 +176,19 @@ library Deposit {
         _revealDeposit(self, fundingTx, reveal, bytes32(0));
     }
 
-    // TODO: Documentation and think about better name.
+    /// @notice Internal function encapsulating the core logic of the deposit
+    ///         reveal process. Handles both regular deposits without extra data
+    ///         as well as deposits with 32-byte extra data embedded in the
+    ///         deposit script. The behavior is controlled by the `extraData`
+    ///         parameter. If `extraData` is bytes32(0), the function triggers
+    ///         the flow for regular deposits. If `extraData` is not bytes32(0),
+    ///         the function triggers the flow for deposits with 32-byte
+    ///         extra data.
+    /// @param fundingTx Bitcoin funding transaction data, see `BitcoinTx.Info`.
+    /// @param reveal Deposit reveal data, see `RevealInfo struct.
+    /// @param extraData 32-byte deposit extra data. Can be bytes32(0).
+    /// @dev Requirements are described in the docstrings of `revealDeposit` and
+    ///      `revealDepositWithExtraData` external functions.
     function _revealDeposit(
         BridgeState.Storage storage self,
         BitcoinTx.Info calldata fundingTx,
@@ -336,7 +367,27 @@ library Deposit {
         );
     }
 
-    // TODO: Documentation and tests.
+    /// @notice Sibling of the `revealDeposit` function. This function allows
+    ///         to reveal a P2(W)SH Bitcoin deposit with 32-byte extra data
+    ///         embedded in the deposit script. The extra data allows to
+    ///         attach additional context to the deposit. For example,
+    ///         it allows a third-party smart contract to reveal the
+    ///         deposit on behalf of the original depositor and provide
+    ///         additional services once the deposit is handled. In this
+    ///         case, the address of the original depositor can be encoded
+    ///         as extra data.
+    /// @param fundingTx Bitcoin funding transaction data, see `BitcoinTx.Info`.
+    /// @param reveal Deposit reveal data, see `RevealInfo struct.
+    /// @param extraData 32-byte deposit extra data.
+    /// @dev Requirements:
+    ///      - All requirements from `revealDeposit` function must be met,
+    ///      - `extraData` must not be bytes32(0),
+    ///      - `extraData` must be the actual extra data used in the P2(W)SH
+    ///        BTC deposit transaction.
+    ///
+    ///      If any of these requirements is not met, the wallet _must_ refuse
+    ///      to sweep the deposit and the depositor has to wait until the
+    ///      deposit script unlocks to receive their BTC back.
     function revealDepositWithExtraData(
         BridgeState.Storage storage self,
         BitcoinTx.Info calldata fundingTx,
