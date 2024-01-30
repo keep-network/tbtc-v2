289251777a632a79b74ea4f4085a6f27e4808d13
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2024-01-30 15:25:12 +0100

    Unit tests for `TBTCDepositorProxy`

---
+++ solidity/contracts/test/TestTBTCDepositorProxy.sol
@@ -0,0 +1,170 @@
+// SPDX-License-Identifier: GPL-3.0-only
+
+pragma solidity ^0.8.0;
+
+import {BTCUtils} from "@keep-network/bitcoin-spv-sol/contracts/BTCUtils.sol";
+
+import {TBTCDepositorProxy} from "../integrator/TBTCDepositorProxy.sol";
+import {IBridge} from "../integrator/IBridge.sol";
+import {ITBTCVault} from "../integrator/ITBTCVault.sol";
+
+import "../bridge/BitcoinTx.sol";
+import "../bridge/Deposit.sol";
+
+contract TestTBTCDepositorProxy is TBTCDepositorProxy {
+    event OnDepositFinalizedCalled(
+        uint256 depositKey,
+        uint256 tbtcAmount,
+        bytes32 extraData
+    );
+
+    function initialize(address _bridge, address _tbtcVault)
+        external
+        initializer
+    {
+        __TBTCDepositorProxy_initialize(_bridge, _tbtcVault);
+    }
+
+    function initializeDepositPublic(
+        BitcoinTx.Info calldata fundingTx,
+        Deposit.DepositRevealInfo calldata reveal,
+        bytes32 extraData
+    ) external {
+        initializeDeposit(fundingTx, reveal, extraData);
+    }
+
+    function finalizeDepositPublic(uint256 depositKey) external {
+        finalizeDeposit(depositKey);
+    }
+
+    function onDepositFinalized(
+        uint256 depositKey,
+        uint256 tbtcAmount,
+        bytes32 extraData
+    ) internal override {
+        emit OnDepositFinalizedCalled(depositKey, tbtcAmount, extraData);
+    }
+}
+
+contract MockBridge is IBridge {
+    using BTCUtils for bytes;
+
+    mapping(uint256 => Deposit.DepositRequest) internal _deposits;
+
+    uint64 internal _depositTxMaxFee = 1 * 1e7; // 0.1 BTC
+
+    event DepositRevealed(uint256 depositKey);
+
+    function revealDepositWithExtraData(
+        BitcoinTx.Info calldata fundingTx,
+        Deposit.DepositRevealInfo calldata reveal,
+        bytes32 extraData
+    ) external {
+        bytes32 fundingTxHash = abi
+            .encodePacked(
+                fundingTx.version,
+                fundingTx.inputVector,
+                fundingTx.outputVector,
+                fundingTx.locktime
+            )
+            .hash256View();
+
+        uint256 depositKey = uint256(
+            keccak256(
+                abi.encodePacked(fundingTxHash, reveal.fundingOutputIndex)
+            )
+        );
+
+        require(
+            _deposits[depositKey].revealedAt == 0,
+            "Deposit already revealed"
+        );
+
+        Deposit.DepositRequest memory request;
+
+        request.depositor = msg.sender;
+        request.amount = uint64(10 * 1e8); // 10 BTC
+        /* solhint-disable-next-line not-rely-on-time */
+        request.revealedAt = uint32(block.timestamp);
+        request.vault = reveal.vault;
+        request.treasuryFee = uint64(1 * 1e8); // 1 BTC
+        request.sweptAt = 0;
+        request.extraData = extraData;
+
+        _deposits[depositKey] = request;
+
+        emit DepositRevealed(depositKey);
+    }
+
+    function sweepDeposit(uint256 depositKey) external {
+        require(_deposits[depositKey].revealedAt != 0, "Deposit not revealed");
+        require(_deposits[depositKey].sweptAt == 0, "Deposit already swept");
+        /* solhint-disable-next-line not-rely-on-time */
+        _deposits[depositKey].sweptAt = uint32(block.timestamp);
+    }
+
+    function deposits(uint256 depositKey)
+        external
+        view
+        returns (Deposit.DepositRequest memory)
+    {
+        return _deposits[depositKey];
+    }
+
+    function depositParameters()
+        external
+        view
+        returns (
+            uint64 depositDustThreshold,
+            uint64 depositTreasuryFeeDivisor,
+            uint64 depositTxMaxFee,
+            uint32 depositRevealAheadPeriod
+        )
+    {
+        depositDustThreshold = 0;
+        depositTreasuryFeeDivisor = 0;
+        depositTxMaxFee = _depositTxMaxFee;
+        depositRevealAheadPeriod = 0;
+    }
+}
+
+contract MockTBTCVault is ITBTCVault {
+    struct Request {
+        uint64 requestedAt;
+        uint64 finalizedAt;
+    }
+
+    mapping(uint256 => Request) internal _requests;
+
+    uint32 public optimisticMintingFeeDivisor = 100; // 1%
+
+    function optimisticMintingRequests(uint256 depositKey)
+        external
+        returns (uint64 requestedAt, uint64 finalizedAt)
+    {
+        Request memory request = _requests[depositKey];
+        return (request.requestedAt, request.finalizedAt);
+    }
+
+    function createOptimisticMintingRequest(uint256 depositKey) external {
+        require(
+            _requests[depositKey].requestedAt == 0,
+            "Request already exists"
+        );
+        /* solhint-disable-next-line not-rely-on-time */
+        _requests[depositKey].requestedAt = uint64(block.timestamp);
+    }
+
+    function finalizeOptimisticMintingRequest(uint256 depositKey) external {
+        require(
+            _requests[depositKey].requestedAt != 0,
+            "Request does not exist"
+        );
+        require(
+            _requests[depositKey].finalizedAt == 0,
+            "Request already finalized"
+        );
+        /* solhint-disable-next-line not-rely-on-time */
+        _requests[depositKey].finalizedAt = uint64(block.timestamp);
+    }
+}
---
+++ solidity/test/integrator/TBTCDepositorProxy.test.ts
@@ -0,0 +1,312 @@
+import { ethers, helpers } from "hardhat"
+import { expect } from "chai"
+import { BigNumber, ContractTransaction } from "ethers"
+import type {
+  MockBridge,
+  MockTBTCVault,
+  TestTBTCDepositorProxy,
+} from "../../typechain"
+import { to1ePrecision } from "../helpers/contract-test-helpers"
+
+const { createSnapshot, restoreSnapshot } = helpers.snapshot
+const { lastBlockTime } = helpers.time
+
+const loadFixture = (vault: string) => ({
+  fundingTx: {
+    version: "0x01000000",
+    inputVector:
+      "0x018348cdeb551134fe1f19d378a8adec9b146671cb67b945b71bf56b20d" +
+      "c2b952f0100000000ffffffff",
+    outputVector:
+      "0x021027000000000000220020bfaeddba12b0de6feeb649af76376876bc1" +
+      "feb6c2248fbfef9293ba3ac51bb4a10d73b00000000001600147ac2d9378a" +
+      "1c47e589dfb8095ca95ed2140d2726",
+    locktime: "0x00000000",
+  },
+  reveal: {
+    fundingOutputIndex: 0,
+    blindingFactor: "0xf9f0c90d00039523",
+    walletPubKeyHash: "0x8db50eb52063ea9d98b3eac91489a90f738986f6",
+    refundPubKeyHash: "0x28e081f285138ccbe389c1eb8985716230129f89",
+    refundLocktime: "0x60bcea61",
+    vault,
+  },
+  extraData:
+    "0xa9b38ea6435c8941d6eda6a46b68e3e2117196995bd154ab55196396b03d9bda",
+  expectedDepositKey:
+    "0xebff13c2304229ab4a97bfbfabeac82c9c0704e4aae2acf022252ac8dc1101d1",
+})
+
+describe("TBTCDepositorProxy", () => {
+  let bridge: MockBridge
+  let tbtcVault: MockTBTCVault
+  let depositorProxy: TestTBTCDepositorProxy
+
+  let fixture
+
+  before(async () => {
+    const MockBridge = await ethers.getContractFactory("MockBridge")
+    bridge = await MockBridge.deploy()
+
+    const MockTBTCVault = await ethers.getContractFactory("MockTBTCVault")
+    tbtcVault = await MockTBTCVault.deploy()
+
+    fixture = loadFixture(tbtcVault.address)
+
+    const TestTBTCDepositorProxy = await ethers.getContractFactory(
+      "TestTBTCDepositorProxy"
+    )
+    depositorProxy = await TestTBTCDepositorProxy.deploy()
+    await depositorProxy.initialize(bridge.address, tbtcVault.address)
+  })
+
+  describe("initializeDeposit", () => {
+    context("when revealed vault does not match", () => {
+      it("should revert", async () => {
+        // Load the fixture with a different vault address.
+        const { fundingTx, reveal, extraData } = loadFixture(
+          ethers.constants.AddressZero
+        )
+
+        await expect(
+          depositorProxy.initializeDepositPublic(fundingTx, reveal, extraData)
+        ).to.be.revertedWith("Vault address mismatch")
+      })
+    })
+
+    context("when revealed vault matches", () => {
+      context("when deposit is rejected by the Bridge", () => {
+        before(async () => {
+          await createSnapshot()
+
+          // Pre-reveal the deposit to cause a revert on the second attempt
+          // made by the TBTCDepositorProxy.
+          await bridge.revealDepositWithExtraData(
+            fixture.fundingTx,
+            fixture.reveal,
+            fixture.extraData
+          )
+        })
+
+        after(async () => {
+          await restoreSnapshot()
+        })
+
+        it("should revert", async () => {
+          await expect(
+            depositorProxy.initializeDepositPublic(
+              fixture.fundingTx,
+              fixture.reveal,
+              fixture.extraData
+            )
+          ).to.be.revertedWith("Deposit already revealed")
+        })
+      })
+
+      context("when deposit is accepted by the Bridge", () => {
+        let tx: ContractTransaction
+
+        before(async () => {
+          await createSnapshot()
+
+          tx = await depositorProxy.initializeDepositPublic(
+            fixture.fundingTx,
+            fixture.reveal,
+            fixture.extraData
+          )
+        })
+
+        after(async () => {
+          await restoreSnapshot()
+        })
+
+        it("should reveal the deposit to the Bridge", async () => {
+          await expect(tx)
+            .to.emit(bridge, "DepositRevealed")
+            .withArgs(fixture.expectedDepositKey)
+        })
+
+        it("should store the deposit as pending", async () => {
+          // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+          expect(
+            await depositorProxy.pendingDeposits(fixture.expectedDepositKey)
+          ).to.be.true
+        })
+
+        it("should emit the DepositInitialized event", async () => {
+          await expect(tx)
+            .to.emit(depositorProxy, "DepositInitialized")
+            .withArgs(fixture.expectedDepositKey, await lastBlockTime())
+        })
+      })
+    })
+  })
+
+  describe("finalizeDeposit", () => {
+    context("when deposit is not initialized", () => {
+      it("should revert", async () => {
+        await expect(
+          depositorProxy.finalizeDepositPublic(fixture.expectedDepositKey)
+        ).to.be.revertedWith("Deposit not initialized")
+      })
+    })
+
+    context("when deposit is already finalized", () => {
+      before(async () => {
+        await createSnapshot()
+
+        await depositorProxy.initializeDepositPublic(
+          fixture.fundingTx,
+          fixture.reveal,
+          fixture.extraData
+        )
+
+        await bridge.sweepDeposit(fixture.expectedDepositKey)
+
+        await depositorProxy.finalizeDepositPublic(fixture.expectedDepositKey)
+      })
+
+      after(async () => {
+        await restoreSnapshot()
+      })
+
+      it("should revert", async () => {
+        await expect(
+          depositorProxy.finalizeDepositPublic(fixture.expectedDepositKey)
+        ).to.be.revertedWith("Deposit not initialized")
+      })
+    })
+
+    context("when deposit is initialized but not finalized yet", () => {
+      before(async () => {
+        await createSnapshot()
+
+        await depositorProxy.initializeDepositPublic(
+          fixture.fundingTx,
+          fixture.reveal,
+          fixture.extraData
+        )
+      })
+
+      after(async () => {
+        await restoreSnapshot()
+      })
+
+      context("when deposit is not finalized by the Bridge", () => {
+        it("should revert", async () => {
+          await expect(
+            depositorProxy.finalizeDepositPublic(fixture.expectedDepositKey)
+          ).to.be.revertedWith("Deposit not finalized by the bridge")
+        })
+      })
+
+      context("when deposit is finalized by the Bridge", () => {
+        // The expected tbtcAmount is calculated as follows:
+        //
+        // - Deposited amount = 10 BTC (hardcoded in MockBridge)
+        // - Treasury fee = 1 BTC (hardcoded in MockBridge)
+        // - Optimistic minting fee = 1%  (hardcoded in MockTBTCVault)
+        // - Transaction max fee = 0.1 BTC (hardcoded in MockBridge)
+        //
+        // ((10 BTC - 1 BTC) * 0.99) - 0.1 BTC = 8.81 BTC = 8.81 * 1e8 sat = 8.81 * 1e18 TBTC
+        const expectedTbtcAmount = to1ePrecision(881, 16).toString()
+
+        context("when the deposit is swept", () => {
+          let tx: ContractTransaction
+
+          before(async () => {
+            await createSnapshot()
+
+            await bridge.sweepDeposit(fixture.expectedDepositKey)
+
+            tx = await depositorProxy.finalizeDepositPublic(
+              fixture.expectedDepositKey
+            )
+          })
+
+          after(async () => {
+            await restoreSnapshot()
+          })
+
+          it("should remove the deposit from pending", async () => {
+            // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+            expect(
+              await depositorProxy.pendingDeposits(fixture.expectedDepositKey)
+            ).to.be.false
+          })
+
+          it("should emit the DepositFinalized event", async () => {
+            await expect(tx)
+              .to.emit(depositorProxy, "DepositFinalized")
+              .withArgs(
+                fixture.expectedDepositKey,
+                expectedTbtcAmount,
+                await lastBlockTime()
+              )
+          })
+
+          it("should call onDepositFinalized with proper params", async () => {
+            await expect(tx)
+              .to.emit(depositorProxy, "OnDepositFinalizedCalled")
+              .withArgs(
+                fixture.expectedDepositKey,
+                expectedTbtcAmount,
+                fixture.extraData
+              )
+          })
+        })
+
+        context("when the deposit is optimistically minted", () => {
+          let tx: ContractTransaction
+
+          before(async () => {
+            await createSnapshot()
+
+            await tbtcVault.createOptimisticMintingRequest(
+              fixture.expectedDepositKey
+            )
+
+            await tbtcVault.finalizeOptimisticMintingRequest(
+              fixture.expectedDepositKey
+            )
+
+            tx = await depositorProxy.finalizeDepositPublic(
+              fixture.expectedDepositKey
+            )
+          })
+
+          after(async () => {
+            await restoreSnapshot()
+          })
+
+          it("should remove the deposit from pending", async () => {
+            // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+            expect(
+              await depositorProxy.pendingDeposits(fixture.expectedDepositKey)
+            ).to.be.false
+          })
+
+          it("should emit the DepositFinalized event", async () => {
+            await expect(tx)
+              .to.emit(depositorProxy, "DepositFinalized")
+              .withArgs(
+                fixture.expectedDepositKey,
+                expectedTbtcAmount,
+                await lastBlockTime()
+              )
+          })
+
+          it("should call onDepositFinalized with proper params", async () => {
+            await expect(tx)
+              .to.emit(depositorProxy, "OnDepositFinalizedCalled")
+              .withArgs(
+                fixture.expectedDepositKey,
+                expectedTbtcAmount,
+                fixture.extraData
+              )
+          })
+        })
+      })
+    })
+  })
+})
