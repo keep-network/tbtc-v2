e927830d725e1ee6aa6e9147e3893df655e6b160
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2024-03-01 12:43:41 +0100

    Deployment artifacts for Base Sepolia
    We are testing the implementation by deploying it on Base Sepolia chain.
    Here are the relevant deployment artifacts.

---
+++ cross-chain/base/contracts/test/BaseL2BitcoinDepositorUpgraded.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0-only
+
+pragma solidity ^0.8.17;
+
+import "@keep-network/tbtc-v2/contracts/l2/L2BitcoinDepositor.sol";
+
+/// @notice L2BitcoinDepositor for Base - upgraded version.
+/// @dev This contract is intended solely for testing purposes.
+contract BaseL2BitcoinDepositorUpgraded is L2BitcoinDepositor {
+    string public newVar;
+
+    function initializeV2(string memory _newVar) public {
+        newVar = _newVar;
+    }
+}
