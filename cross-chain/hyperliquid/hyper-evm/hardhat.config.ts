cab793466c9176de7467c677b7186c33041316de
evandrosaturnino <evandro.saturnino@gmail.com>
2025-02-28 01:39:29 -0300

    feat: add hyperevm deployment script for the tbtc token

--- cross-chain/hyperliquid/hyper-evm/hardhat.config.ts
+++ cross-chain/hyperliquid/hyper-evm/hardhat.config.ts
@@ -31,36 +31,82 @@ const config: HardhatUserConfig = {
   networks: {
     hardhat: {
       deploy: [
-        "deploy",
+        // "deploy_l1",
+        "deploy_hl",
       ],
+    },
+    mainnet: {
+      url: process.env.L1_CHAIN_API_URL || "",
+      chainId: 1,
+      deploy: ["deploy_l1"],
+      accounts: process.env.L1_ACCOUNTS_PRIVATE_KEYS
+        ? process.env.L1_ACCOUNTS_PRIVATE_KEYS.split(",")
+        : undefined,
+      tags: ["etherscan"],
+    },
+    sepolia: {
+      url: process.env.L1_CHAIN_API_URL || "",
+      chainId: 11155111,
+      deploy: ["deploy_l1"],
+      accounts: process.env.L1_ACCOUNTS_PRIVATE_KEYS
+        ? process.env.L1_ACCOUNTS_PRIVATE_KEYS.split(",")
+        : undefined,
+      tags: ["etherscan"],
+    },
+    hyperEvmMainnet: {
+      url: process.env.HYPER_EVM_API_URL || "",
+      chainId: 999,
+      deploy: ["deploy_hl"],
+      accounts: process.env.HL_ACCOUNTS_PRIVATE_KEYS
+        ? process.env.HL_ACCOUNTS_PRIVATE_KEYS.split(",")
+        : undefined,
     },
     hyperEvmTestnet: {
       url: process.env.HYPER_EVM_API_URL || "",
       chainId: 998,
-      deploy: ["deploy"],
-      accounts: process.env.ACCOUNTS_PRIVATE_KEYS
-        ? process.env.ACCOUNTS_PRIVATE_KEYS.split(",")
+      deploy: ["deploy_hl"],
+      accounts: process.env.HL_ACCOUNTS_PRIVATE_KEYS
+        ? process.env.HL_ACCOUNTS_PRIVATE_KEYS.split(",")
         : undefined,
     },
   },
 
-  // external: {
-  //   deployments: {
-  //     hyperEvmTestnet: ["./external/hyperEvmTestnet"],
-  //   },
-  // },
+  external: {
+    deployments: {
+      mainnet: ["./external/mainnet"],
+      sepolia: ["./external/sepolia"],
+      hyperEvmMainnet: ["./external/hyperEvmMainnet"],
+      hyperEvmTestnet: ["./external/hyperEvmTestnet"],
+    },
+  },
 
   deploymentArtifactsExport: {
-    hyperEvmTestnet: "artifacts/",
+    mainnet: "artifacts/l1",
+    sepolia: "artifacts/l1",
+    hyperEvmMainnet: "artifacts/hl",
+    hyperEvmTestnet: "artifacts/hl",
+  },
+
+  etherscan: {
+    apiKey: {
+      sepolia: process.env.ETHERSCAN_API_KEY,
+      mainnet: process.env.ETHERSCAN_API_KEY,
+    }
   },
 
   namedAccounts: {
     deployer: {
       default: 1,
+      mainnet: "0x15424dC94D4da488DB0d0e0B7aAdB86835813a63",
+      sepolia: "0x15424dC94D4da488DB0d0e0B7aAdB86835813a63",
+      hyperEvmMainnet: "0x15424dC94D4da488DB0d0e0B7aAdB86835813a63",
       hyperEvmTestnet: "0x15424dC94D4da488DB0d0e0B7aAdB86835813a63",
     },
     governance: {
       default: 2,
+      mainnet: "0x9f6e831c8f8939dc0c830c6e492e7cef4f9c2f5f",
+      sepolia: "0x9f6e831c8f8939dc0c830c6e492e7cef4f9c2f5f",
+      hyperEvmMainnet: "0x9f6e831c8f8939dc0c830c6e492e7cef4f9c2f5f",
       hyperEvmTestnet: "0x9f6e831c8f8939dc0c830c6e492e7cef4f9c2f5f",
     },
   },
